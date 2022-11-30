6d7700a64ff1f6703861172b5c5b3ed892d934bb
limgo badge action <mengel@redhat.com>
2022-11-30 09:45:49 +0100

    added controller and event loop to orchestrator

---
+++ src/orch/controller.c
@@ -0,0 +1,83 @@
+#include "controller.h"
+
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int create_master_socket(int port) {
+	struct sockaddr_in servaddr;
+
+	int fd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK, 0);
+	if (fd < 0) {
+		int errsv = errno;
+		fprintf(stderr, "Failed to create socket: %m\n");
+		return -errsv;
+	}
+
+	int yes = 1;
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		int errsv = errno;
+		fprintf(stderr, "Failed to create socket: %m\n");
+		return -errsv;
+	}
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(port);
+
+	if (bind(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+		int errsv = errno;
+		fprintf(stderr, "Failed to bind socket: %m\n");
+		return -errsv;
+	}
+
+	if ((listen(fd, SOMAXCONN)) != 0) {
+		int errsv = errno;
+		fprintf(stderr, "Failed to listed socket: %m\n");
+		return -errsv;
+	}
+
+	return fd;
+}
+
+static int accept_handler(sd_event_source *s, int fd, uint32_t revents,
+			  void *userdata) {
+	int nfd = accept4(fd, NULL, NULL, SOCK_NONBLOCK | SOCK_CLOEXEC);
+	if (nfd < 0) {
+		if (errno == EAGAIN || errno == EINTR || errno == EWOULDBLOCK)
+			return 0;
+		else {
+			int errsv = errno;
+			fprintf(stderr, "Failed to accept: %m\n");
+			return -errsv;
+		}
+	}
+
+	// TODO: setup peer dbus, incl. vtable with services, signals, properties
+
+	return 0;
+}
+
+int controller_setup(int port, sd_event *event, sd_event_source *event_source) {
+	int accept_fd = create_master_socket(port);
+
+	int r = sd_event_add_io(event, &event_source, accept_fd, EPOLLIN,
+				accept_handler, NULL);
+	if (r < 0) {
+		fprintf(stderr, "Failed to add io event: %s\n", strerror(-r));
+		return -1;
+	}
+
+	r = sd_event_source_set_io_fd_own(event_source, true);
+	if (r < 0) {
+		fprintf(stderr, "Failed to set io fd own: %s\n", strerror(-r));
+		return -1;
+	}
+
+	(void)sd_event_source_set_description(event_source, "master-socket");
+
+	return 0;
+}
--- src/orch/orch.c
+++ src/orch/orch.c
@@ -1,7 +1,47 @@
+#include "../common/dbus.h"
+#include "controller.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int getArgs(int argc, char *argv[], int *ctrlPort) {
+	if (argc < 2) {
+		fprintf(stderr, "No controller port given\n");
+		return -1;
+	}
+	*ctrlPort = atoi(argv[1]);
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
 	fprintf(stdout, "Hello from orchestrator!\n");
+
+	int port;
+	if (getArgs(argc, argv, &port) == -1) {
+		return EXIT_FAILURE;
+	}
+
+	int r;
+	_cleanup_sd_event_ sd_event *event = NULL;
+	r = sd_event_default(&event);
+	if (r < 0) {
+		fprintf(stderr, "Failed to create event: %s\n", strerror(-r));
+		return EXIT_FAILURE;
+	}
+
+	_cleanup_sd_event_source_ sd_event_source *event_source = NULL;
+	r = controller_setup(port, event, event_source);
+	if (r < 0) {
+		fprintf(stderr, "Failed to setup controller");
+		return EXIT_FAILURE;
+	}
+
+	r = sd_event_loop(event);
+	if (r < 0) {
+		fprintf(stderr, "Event loop failed: %s\n", strerror(-r));
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
