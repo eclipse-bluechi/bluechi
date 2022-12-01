536b4d0ef70977bd6e8798fba7d2cb5a93906f8f
limgo badge action <mengel@redhat.com>
2022-12-01 16:06:04 +0100

    added cli options for node

--- src/node/node.c
+++ src/node/node.c
@@ -1,7 +1,20 @@
+#include "opt.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
         fprintf(stdout, "Hello from node!\n");
+
+        struct sockaddr_in host;
+        memset(&host, 0, sizeof(host));
+        host.sin_family = AF_INET;
+        host.sin_addr.s_addr = htonl(INADDR_ANY);
+        host.sin_port = 0;
+
+        get_opts(argc, argv, &host);
+
+
         return EXIT_SUCCESS;
 }
---
+++ src/node/opt.c
@@ -0,0 +1,45 @@
+#include "opt.h"
+#include "../common/parse-util.h"
+
+#include <arpa/inet.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_opts(int argc, char *argv[], struct sockaddr_in *orchAddr) {
+        int opt, r, port;
+        int hflag, pflag;
+        while ((opt = getopt(argc, argv, "h:p:")) != -1) {
+                switch (opt) {
+                case 'h':
+                        r = inet_pton(AF_INET, optarg, &(orchAddr->sin_addr));
+                        if (r < 1) {
+                                fprintf(stderr, "Invalid host option '%s'\n", optarg);
+                                exit(EXIT_FAILURE);
+                        }
+
+                        hflag = 1;
+                        break;
+                case 'p':
+                        if (parse_long(optarg, (long *) &port) != 0) {
+                                fprintf(stderr, "Invalid port option '%s'\n", optarg);
+                                exit(EXIT_FAILURE);
+                        }
+                        orchAddr->sin_port = htons(port);
+
+                        pflag = 1;
+                        break;
+                default:
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+        if (hflag != 1) {
+                fprintf(stderr, "Missing host option. Usage: %s [-h host] [-p port]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+        if (pflag != 1) {
+                fprintf(stderr, "Missing port option. Usage: %s [-h host] [-p port]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+}
