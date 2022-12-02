4391750bf1e05f21b0e28f2728cb17c334893d5c
Veronika Fuxova <vfuxova@redhat.com>
2022-12-02 16:14:11 +0100

    Fmt ini.h and ini.c
    Signed-off-by: Veronika Fuxova <vfuxova@redhat.com>

--- src/ini/ini.h
+++ src/ini/ini.h
@@ -12,42 +12,39 @@ extern "C" {
 
 /* Nonzero if ini_handler callback should accept lineno parameter. */
 #ifndef INI_HANDLER_LINENO
-#define INI_HANDLER_LINENO 0
+#        define INI_HANDLER_LINENO 0
 #endif
 
 /* Visibility symbols, required for Windows DLLs */
 #ifndef INI_API
-#if defined _WIN32 || defined __CYGWIN__
-#	ifdef INI_SHARED_LIB
-#		ifdef INI_SHARED_LIB_BUILDING
-#			define INI_API __declspec(dllexport)
-#		else
-#			define INI_API __declspec(dllimport)
-#		endif
-#	else
-#		define INI_API
-#	endif
-#else
-#	if defined(__GNUC__) && __GNUC__ >= 4
-#		define INI_API __attribute__ ((visibility ("default")))
-#	else
-#		define INI_API
-#	endif
-#endif
+#        if defined _WIN32 || defined __CYGWIN__
+#                ifdef INI_SHARED_LIB
+#                        ifdef INI_SHARED_LIB_BUILDING
+#                                define INI_API __declspec(dllexport)
+#                        else
+#                                define INI_API __declspec(dllimport)
+#                        endif
+#                else
+#                        define INI_API
+#                endif
+#        else
+#                if defined(__GNUC__) && __GNUC__ >= 4
+#                        define INI_API __attribute__((visibility("default")))
+#                else
+#                        define INI_API
+#                endif
+#        endif
 #endif
 
 /* Typedef for prototype of handler function. */
 #if INI_HANDLER_LINENO
-typedef int (*ini_handler)(void* user, const char* section,
-                           const char* name, const char* value,
-                           int lineno);
+typedef int (*ini_handler)(void *user, const char *section, const char *name, const char *value, int lineno);
 #else
-typedef int (*ini_handler)(void* user, const char* section,
-                           const char* name, const char* value);
+typedef int (*ini_handler)(void *user, const char *section, const char *name, const char *value);
 #endif
 
 /* Typedef for prototype of fgets-style reader function. */
-typedef char* (*ini_reader)(char* str, int num, void* stream);
+typedef char *(*ini_reader)(char *str, int num, void *stream);
 
 /* Parse given INI-style file. May have [section]s, name=value pairs
    (whitespace stripped), and comments starting with ';' (semicolon). Section
@@ -60,93 +57,92 @@ typedef char* (*ini_reader)(char* str, int num, void* stream);
    stop on first error), -1 on file open error, or -2 on memory allocation
    error (only when INI_USE_STACK is zero).
 */
-INI_API int ini_parse(const char* filename, ini_handler handler, void* user);
+INI_API int ini_parse(const char *filename, ini_handler handler, void *user);
 
 /* Same as ini_parse(), but takes a FILE* instead of filename. This doesn't
    close the file when it's finished -- the caller must do that. */
-INI_API int ini_parse_file(FILE* file, ini_handler handler, void* user);
+INI_API int ini_parse_file(FILE *file, ini_handler handler, void *user);
 
 /* Same as ini_parse(), but takes an ini_reader function pointer instead of
    filename. Used for implementing custom or string-based I/O (see also
    ini_parse_string). */
-INI_API int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
-                     void* user);
+INI_API int ini_parse_stream(ini_reader reader, void *stream, ini_handler handler, void *user);
 
 /* Same as ini_parse(), but takes a zero-terminated string with the INI data
 instead of a file. Useful for parsing INI data from a network socket or
 already in memory. */
-INI_API int ini_parse_string(const char* string, ini_handler handler, void* user);
+INI_API int ini_parse_string(const char *string, ini_handler handler, void *user);
 
 /* Nonzero to allow multi-line value parsing, in the style of Python's
    configparser. If allowed, ini_parse() will call the handler with the same
    name for each subsequent line parsed. */
 #ifndef INI_ALLOW_MULTILINE
-#define INI_ALLOW_MULTILINE 1
+#        define INI_ALLOW_MULTILINE 1
 #endif
 
 /* Nonzero to allow a UTF-8 BOM sequence (0xEF 0xBB 0xBF) at the start of
    the file. See https://github.com/benhoyt/inih/issues/21 */
 #ifndef INI_ALLOW_BOM
-#define INI_ALLOW_BOM 1
+#        define INI_ALLOW_BOM 1
 #endif
 
 /* Chars that begin a start-of-line comment. Per Python configparser, allow
    both ; and # comments at the start of a line by default. */
 #ifndef INI_START_COMMENT_PREFIXES
-#define INI_START_COMMENT_PREFIXES ";#"
+#        define INI_START_COMMENT_PREFIXES ";#"
 #endif
 
 /* Nonzero to allow inline comments (with valid inline comment characters
    specified by INI_INLINE_COMMENT_PREFIXES). Set to 0 to turn off and match
    Python 3.2+ configparser behaviour. */
 #ifndef INI_ALLOW_INLINE_COMMENTS
-#define INI_ALLOW_INLINE_COMMENTS 1
+#        define INI_ALLOW_INLINE_COMMENTS 1
 #endif
 #ifndef INI_INLINE_COMMENT_PREFIXES
-#define INI_INLINE_COMMENT_PREFIXES ";"
+#        define INI_INLINE_COMMENT_PREFIXES ";"
 #endif
 
 /* Nonzero to use stack for line buffer, zero to use heap (malloc/free). */
 #ifndef INI_USE_STACK
-#define INI_USE_STACK 1
+#        define INI_USE_STACK 1
 #endif
 
 /* Maximum line length for any line in INI file (stack or heap). Note that
    this must be 3 more than the longest line (due to '\r', '\n', and '\0'). */
 #ifndef INI_MAX_LINE
-#define INI_MAX_LINE 200
+#        define INI_MAX_LINE 200
 #endif
 
 /* Nonzero to allow heap line buffer to grow via realloc(), zero for a
    fixed-size buffer of INI_MAX_LINE bytes. Only applies if INI_USE_STACK is
    zero. */
 #ifndef INI_ALLOW_REALLOC
-#define INI_ALLOW_REALLOC 0
+#        define INI_ALLOW_REALLOC 0
 #endif
 
 /* Initial size in bytes for heap line buffer. Only applies if INI_USE_STACK
    is zero. */
 #ifndef INI_INITIAL_ALLOC
-#define INI_INITIAL_ALLOC 200
+#        define INI_INITIAL_ALLOC 200
 #endif
 
 /* Stop parsing on first error (default is to keep parsing). */
 #ifndef INI_STOP_ON_FIRST_ERROR
-#define INI_STOP_ON_FIRST_ERROR 0
+#        define INI_STOP_ON_FIRST_ERROR 0
 #endif
 
 /* Nonzero to call the handler at the start of each new section (with
    name and value NULL). Default is to only call the handler on
    each name=value pair. */
 #ifndef INI_CALL_HANDLER_ON_NEW_SECTION
-#define INI_CALL_HANDLER_ON_NEW_SECTION 0
+#        define INI_CALL_HANDLER_ON_NEW_SECTION 0
 #endif
 
 /* Nonzero to allow a name without a value (no '=' or ':' on the line) and
    call the handler with value NULL in this case. Default is to treat
    no-value lines as an error. */
 #ifndef INI_ALLOW_NO_VALUE
-#define INI_ALLOW_NO_VALUE 0
+#        define INI_ALLOW_NO_VALUE 0
 #endif
 
 /* Nonzero to use custom ini_malloc, ini_free, and ini_realloc memory
@@ -154,7 +150,7 @@ INI_API int ini_parse_string(const char* string, ini_handler handler, void* user
    have the same signatures as malloc/free/realloc and behave in a similar
    way. ini_realloc is only needed if INI_ALLOW_REALLOC is set. */
 #ifndef INI_CUSTOM_ALLOCATOR
-#define INI_CUSTOM_ALLOCATOR 0
+#        define INI_CUSTOM_ALLOCATOR 0
 #endif
 
 
