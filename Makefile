6d5a057b97711974e1199999147ec54ee95a4f37
Michael Engel <mengel@redhat.com>
2023-01-09 13:41:41 +0100

    Added config.h and config.c with parsing function for nodes and orch. (#47)
    * Added config.h and config.c with parsing function for nodes and orch.
    Used inih for parsing. For parsing ini file use
    parsing_ini_file_orch or parsing_ini_file_node.
    Also added structure configuration_node and
    configuration_orch to save the ini file data.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>
    Co-authored-by: ArtiomDivak <adivak@redhat.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-ALL_SRC_FILES = 'find . -name "*.[ch]" ! -path "./binchihua/src/ini/*" ! -path "./binchihua/test/**" ! -path "./builddir/**" -print0'
+ALL_SRC_FILES = 'find . -name "*.[ch]" ! -path "./binchihua/src/ini/ini.[ch]" ! -path "./binchihua/src/common/hashmap/*" ! -path "./binchihua/test/**" ! -path "./builddir/**" -print0'
 
 fmt:
 	eval $(ALL_SRC_FILES) | xargs -0 -I {} clang-format -i --sort-includes {}
---
+++ binchihua/src/ini/config.h
@@ -0,0 +1,19 @@
+#include "../common/hashmap/hashmap.h"
+
+typedef struct {
+        char *key;
+        char *value;
+} keyValue;
+
+typedef struct {
+        char *topic;
+        struct hashmap *keys_and_values;
+} topic;
+
+
+struct hashmap *parsing_ini_file(const char *file);
+void free_topics_hashmap(struct hashmap **topics);
+
+void print_all_topics(struct hashmap *topics);
+
+#define _cleanup_hashmap_ __attribute__((__cleanup__(free_topics_hashmap)))
