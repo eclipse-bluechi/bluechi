bbba63c2a4b362b8f5c12d12924c2ca392d48917
Steve Dunnagan <sdunnaga@redhat.com>
2023-01-13 15:26:31 -0500

    Add signal handler for client program.

--- binchihua/src/client/client.c
+++ binchihua/src/client/client.c
@@ -1,7 +1,32 @@
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+// NOLINTNEXTLINE
+static bool done = false;
+
+static void signal_handler(int signal) {
+        printf("\n%s(): Received signal %d\n", __func__, signal);
+        done = true;
+}
 
 int main() {
-        fprintf(stdout, "Hello from client!\n");
-        return EXIT_SUCCESS;
+        struct sigaction sa;
+        sa.sa_handler = &signal_handler;
+        sigaction(SIGINT, &sa, NULL);
+        sigaction(SIGTERM, &sa, NULL);
+        sigaction(SIGSEGV, &sa, NULL);
+        sigaction(SIGBUS, &sa, NULL);
+        sigaction(SIGABRT, &sa, NULL);
+
+        while (!done) {
+                sleep(1);
+                printf("%s(): Hello from client\n", __func__);
+        }
+
+        printf("%s(): Goodbye fromt client\n", __func__);
+
+        exit(EXIT_SUCCESS);
 }
