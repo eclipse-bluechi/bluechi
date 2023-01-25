4a0f169a181d074901c5b9399b4bd27b65d7dac1
Michael Engel <mengel@redhat.com>
2023-01-25 16:03:19 +0100

    removed client example for shutdown service
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- hirte/src/client/client.c
+++ hirte/src/client/client.c
@@ -4,10 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "../../../libhirte/include/bus/user-bus.h"
-#include "../../../libhirte/include/orchestrator.h"
-#include "../../../libhirte/include/service/shutdown.h"
-
 // NOLINTNEXTLINE
 static bool done = false;
 
@@ -17,9 +13,6 @@ static void signal_handler(int signal) {
 }
 
 int main() {
-        sd_bus *user_bus = user_bus_open(NULL);
-        service_call_shutdown(user_bus, ORCHESTRATOR_SERVICE_DEFAULT_NAME);
-
         struct sigaction sa;
         sa.sa_handler = &signal_handler;
         sigaction(SIGINT, &sa, NULL);
