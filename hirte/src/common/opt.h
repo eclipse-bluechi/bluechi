7035f9df3e9e71ccd21abf7c948dc97dca690a05
Alexander Larsson <alexl@redhat.com>
2023-01-26 11:07:31 +0100

    Move node.c from library into node dir and simplify setup
    The node object will only ever be called from the node binary, so no
    need to put it in the library.
    
    We consolidated the option parsing into a main.c, and which just
    creates an empty Node object, calls individual methods to set any
    options that was passed and then calls start(), which does all
    the actual work.
    
    This also adds config file parsing, and support for a name option.
    
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- hirte/src/common/opt.h
+++ hirte/src/common/opt.h
@@ -3,10 +3,21 @@
 // NOLINTBEGIN(modernize-macro-to-enum)
 #define ARG_PORT "port"
 #define ARG_PORT_SHORT 'P'
+#define ARG_PORT_SHORT_S "P:"
+
 #define ARG_HOST "host"
 #define ARG_HOST_SHORT 'H'
+#define ARG_HOST_SHORT_S "H:"
+
 #define ARG_CONFIG "config"
 #define ARG_CONFIG_SHORT 'c'
+#define ARG_CONFIG_SHORT_S "c:"
+
+#define ARG_NAME "name"
+#define ARG_NAME_SHORT 'n'
+#define ARG_NAME_SHORT_S "n:"
+
 #define ARG_HELP "help"
 #define ARG_HELP_SHORT 'h'
+#define ARG_HELP_SHORT_S "h"
 // NOLINTEND(modernize-macro-to-enum)
--- hirte/src/node/node.c
+++ hirte/src/node/node.c
@@ -1,55 +1,244 @@
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include "../../../libhirte/src/node.h"
+#include "../../../libhirte/src/bus/peer-bus.h"
+#include "../../../libhirte/src/bus/systemd-bus.h"
+#include "../../../libhirte/src/bus/user-bus.h"
+#include "../../../libhirte/src/common/dbus.h"
 #include "../../../libhirte/src/service/shutdown.h"
+#include "../common/opt.h"
+#include "../common/parse-util.h"
 #include "../ini/config.h"
-#include "opt.h"
+#include "../common/parse-util.h"
+#include "node.h"
 
+Node *node_new(void) {
+        fprintf(stdout, "Creating Node...\n");
 
-int main(int argc, char *argv[]) {
-        int r = -1;
-
-        fprintf(stdout, "Hello from node!\n");
-
-        struct sockaddr_in host;
-        memset(&host, 0, sizeof(host));
-        host.sin_family = AF_INET;
-        host.sin_addr.s_addr = htonl(INADDR_ANY);
-        host.sin_port = 0;
-
-        get_opts(argc, argv, &host);
-
-        char *ini_file_location = NULL;
-        struct hashmap *ini_hashmap = NULL;
-        ini_hashmap = parsing_ini_file(ini_file_location);
-        if (ini_hashmap == NULL) {
-                return EXIT_FAILURE;
+        int r = 0;
+        _cleanup_sd_event_ sd_event *event = NULL;
+        r = sd_event_default(&event);
+        if (r < 0) {
+                fprintf(stderr, "Failed to create event loop: %s\n", strerror(-r));
+                return NULL;
         }
 
-        _cleanup_node_ Node *node = node_new(&host, NODE_SERVICE_DEFAULT_NAME);
+        _cleanup_free_ char *service_name = strdup(NODE_SERVICE_DEFAULT_NAME);
+        if (service_name == NULL) {
+                fprintf(stderr, "Out of memory\n");
+                return NULL;
+        }
+
+        Node *n = malloc0(sizeof(Node));
+        n->ref_count = 1;
+        n->event = steal_pointer(&event);
+        n->user_bus_service_name = steal_pointer(&service_name);
+        n->port = NODE_DEFAULT_PORT;
+
+        return n;
+}
+
+Node *node_ref(Node *node) {
+        node->ref_count++;
+        return node;
+}
+
+void node_unref(Node *node) {
+        node->ref_count--;
+        if (node->ref_count != 0) {
+                return;
+        }
+
+        free(node->name);
+        free(node->host);
+        free(node->orch_addr);
+        free(node->user_bus_service_name);
+
+        if (node->event != NULL) {
+                sd_event_unrefp(&node->event);
+        }
+
+        if (node->peer_dbus != NULL) {
+                sd_bus_unrefp(&node->peer_dbus);
+        }
+        if (node->user_dbus != NULL) {
+                sd_bus_unrefp(&node->user_dbus);
+        }
+        if (node->systemd_dbus != NULL) {
+                sd_bus_unrefp(&node->systemd_dbus);
+        }
+
+        free(node);
+}
+
+void node_unrefp(Node **nodep) {
+        if (nodep && *nodep) {
+                node_unref(*nodep);
+                *nodep = NULL;
+        }
+}
+
+bool node_set_port(Node *node, const char *port_s) {
+        uint16_t port = 0;
+
+        if (!parse_port(port_s, &port)) {
+                fprintf(stderr, "Invalid port format '%s'\n", port_s);
+                return false;
+        }
+        node->port = port;
+        return true;
+}
+
+bool node_set_host(Node *node, const char *host) {
+        char *dup = strdup(host);
+        if (dup == NULL) {
+                fprintf(stderr, "Out of memory\n");
+                return false;
+        }
+        free(node->host);
+        node->host = dup;
+        return true;
+}
+
+bool node_set_name(Node *node, const char *name) {
+        char *dup = strdup(name);
+        if (dup == NULL) {
+                fprintf(stderr, "Out of memory\n");
+                return false;
+        }
+
+        free(node->name);
+        node->name = dup;
+        return true;
+}
+
+bool node_parse_config(Node *node, const char *configfile) {
+        _cleanup_hashmap_ struct hashmap *ini_hashmap = NULL;
+        struct hashmap *node_hashmap = NULL;
+        const char *name, *host, *port;
+
+        ini_hashmap = parsing_ini_file(configfile);
+        if (ini_hashmap == NULL) {
+                return false;
+        }
+
+        node_hashmap = hashmap_get(ini_hashmap, "Node");
+
+        if (node_hashmap == NULL) {
+                return true;
+        }
+
+        name = hashmap_get(node_hashmap, "Name");
+        if (name) {
+                if (!node_set_name(node, name)) {
+                        return false;
+                }
+        }
+
+        host = hashmap_get(node_hashmap, "Host");
+        if (host) {
+                if (!node_set_host(node, host)) {
+                        return false;
+                }
+        }
+
+        port = hashmap_get(node_hashmap, "Port");
+        if (port) {
+                if (!node_set_host(node, port)) {
+                        return false;
+                }
+        }
+
+        return true;
+}
+
+
+bool node_start(Node *node) {
+        struct sockaddr_in host;
+        int r;
+
+        fprintf(stdout, "Starting Node...\n");
+
         if (node == NULL) {
-                return EXIT_FAILURE;
+                return false;
+        }
+
+        memset(&host, 0, sizeof(host));
+        host.sin_family = AF_INET;
+        host.sin_port = htons(node->port);
+
+        if (node->name == NULL) {
+                fprintf(stderr, "No name specified\n");
+                return false;
+        }
+
+        if (node->host == NULL) {
+                fprintf(stderr, "No host specified\n");
+                return false;
+        }
+
+        r = inet_pton(AF_INET, node->host, &host.sin_addr);
+        if (r < 1) {
+                fprintf(stderr, "Invalid host option '%s'\n", optarg);
+                return false;
+        }
+
+        node->orch_addr = assemble_tcp_address(&host);
+        if (node->orch_addr == NULL) {
+                return false;
+        }
+
+        node->user_dbus = user_bus_open(node->event);
+        if (node->user_dbus == NULL) {
+                fprintf(stderr, "Failed to open user dbus\n");
+                return false;
+        }
+
+        r = sd_bus_request_name(node->user_dbus, node->user_bus_service_name, SD_BUS_NAME_REPLACE_EXISTING);
+        if (r < 0) {
+                fprintf(stderr, "Failed to acquire service name on user dbus: %s\n", strerror(-r));
+                return false;
+        }
+
+        node->systemd_dbus = systemd_bus_open(node->event);
+        if (node->systemd_dbus == NULL) {
+                fprintf(stderr, "Failed to open systemd dbus\n");
+                return false;
+        }
+
+        node->peer_dbus = peer_bus_open(node->event, "peer-bus-to-orchestrator", node->orch_addr);
+        if (node->peer_dbus == NULL) {
+                fprintf(stderr, "Failed to open peer dbus\n");
+                return false;
         }
 
         r = shutdown_service_register(node->user_dbus, node->event);
         if (r < 0) {
                 fprintf(stderr, "Failed to register shutdown service\n");
-                return EXIT_FAILURE;
+                return false;
         }
 
         r = event_loop_add_shutdown_signals(node->event);
         if (r < 0) {
                 fprintf(stderr, "Failed to add signals to node event loop\n");
-                return EXIT_FAILURE;
+                return false;
         }
 
-        if (node_start(node)) {
-                return EXIT_SUCCESS;
+        r = sd_event_loop(node->event);
+        if (r < 0) {
+                fprintf(stderr, "Starting event loop failed: %s\n", strerror(-r));
+                return false;
         }
 
-        fprintf(stdout, "Node exited\n");
+        return true;
+}
 
-        return EXIT_FAILURE;
+bool node_stop(const Node *node) {
+        fprintf(stdout, "Stopping Node...\n");
+
+        if (node == NULL) {
+                return false;
+        }
+
+        return true;
 }
