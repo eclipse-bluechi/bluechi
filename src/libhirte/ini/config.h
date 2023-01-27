7b67651ef15a629f8f3a4d02b3ff8a70839b19ba
Alexander Larsson <alexl@redhat.com>
2023-01-27 14:24:38 +0100

    Fix some issues with ini parser and add a nicer api
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- src/libhirte/ini/config.h
+++ src/libhirte/ini/config.h
@@ -2,20 +2,24 @@
 
 #include "libhirte/hashmap/hashmap.h"
 
-typedef struct {
-        char *key;
-        char *value;
-} keyValue;
-
-typedef struct {
-        char *topic;
-        struct hashmap *keys_and_values;
-} topic;
+typedef struct hashmap config;
+typedef struct topic topic;
 
 
-struct hashmap *parsing_ini_file(const char *file);
-void free_topics_hashmap(struct hashmap **topics);
+config *parsing_ini_file(const char *file);
+config *new_config(void);
+void free_config(config *config);
+void free_configp(config **configp);
 
-void print_all_topics(struct hashmap *topics);
+void print_all_topics(config *config);
+topic *config_lookup_topic(config *config, const char *name);
+topic *config_ensure_topic(config *config, const char *name);
+const char *config_lookup(config *config, const char *topicname, const char *key);
 
-#define _cleanup_hashmap_ __attribute__((__cleanup__(free_topics_hashmap)))
+const char *topic_lookup(topic *t, const char *key);
+bool topic_set(topic *t, const char *key, const char *value);
+
+void free_hashmapp(struct hashmap **hmp);
+
+#define _cleanup_hashmap_ __attribute__((__cleanup__(free_hashmap)))
+#define _cleanup_config_ __attribute__((__cleanup__(free_configp)))
