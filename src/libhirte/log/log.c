a8e2a14ccf2282f0a42b67888e287da4273c2674
Michael Engel <mengel@redhat.com>
2023-02-06 14:03:06 +0100

    moved log implementation to .c file
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ src/libhirte/log/log.c
@@ -0,0 +1,72 @@
+#include "log.h"
+
+static const char * const log_level_strings[] = { "DEBUG", "INFO", "WARN", "ERROR" };
+
+const char *log_level_to_string(LogLevel l) {
+        return log_level_strings[l];
+}
+
+
+void hirte_log_to_journald_with_location(
+                LogLevel lvl,
+                const char *file,
+                const char *line,
+                const char *func,
+                const char *msg,
+                const char *data) {
+        // file and line are required to contain the field names and func gets its key
+        // automatically set (in contrast to what is written in the documentation).
+        // see https://www.freedesktop.org/software/systemd/man/sd_journal_print.html
+        _cleanup_free_ char *file_info = NULL;
+        _cleanup_free_ char *line_info = NULL;
+        asprintf(&file_info, "CODE_FILE=%s", file);
+        asprintf(&line_info, "CODE_LINE=%s", line);
+
+        // clang-format off
+        sd_journal_send_with_location(
+                file_info, line_info, func,
+                "HIRTE_LOG_LEVEL=%s", log_level_to_string(lvl),
+                "HIRTE_MESSAGE=%s", msg,
+                "HIRTE_DATA=%s", data,
+                NULL);
+        // clang-format on
+}
+
+void hirte_log_to_stdout_with_location(
+                LogLevel lvl,
+                const char *file,
+                const char *line,
+                const char *func,
+                const char *msg,
+                const char *data) {
+
+        time_t t = time(NULL);
+        const size_t timestamp_size = 9;
+        char timebuf[timestamp_size];
+        timebuf[strftime(timebuf, sizeof(timebuf), "%H:%M:%S", localtime(&t))] = '\0';
+
+        // clang-format off
+        fprintf(stderr, "%s %s\t%s:%s %s\t{msg: %s, data: %s}\n", 
+                timebuf, log_level_to_string(lvl), 
+                file, line, func,
+                msg, data);
+        // clang-format on
+}
+
+struct HirteLogConfig config;
+
+void hirte_log_set_log_fn(LogFn log_fn) {
+        config.log_fn = log_fn;
+}
+
+void hirte_log_set_level(LogLevel level) {
+        config.level = level;
+}
+
+void hirte_log_set_quiet(bool is_quiet) {
+        config.is_quiet = is_quiet;
+}
+
+bool shouldLog(LogLevel lvl) {
+        return (config.level <= lvl && !config.is_quiet && config.log_fn != NULL);
+}
--- src/libhirte/log/log.h
+++ src/libhirte/log/log.h
@@ -14,12 +14,7 @@ typedef enum LogLevel {
         LOG_LEVEL_ERROR,
 } LogLevel;
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-static const char *log_level_strings[] = { "DEBUG", "INFO", "WARN", "ERROR" };
-
-const char *log_level_to_string(LogLevel l) {
-        return log_level_strings[l];
-}
+const char *log_level_to_string(LogLevel l);
 
 
 typedef void (*LogFn)(
@@ -30,84 +25,39 @@ typedef void (*LogFn)(
                 const char *msg,
                 const char *data);
 
-// const char *file, const char *line, const char *func,
 void hirte_log_to_journald_with_location(
                 LogLevel lvl,
                 const char *file,
                 const char *line,
                 const char *func,
                 const char *msg,
-                const char *data) {
-        // file and line are required to contain the field names and func gets its key
-        // automatically set (in contrast to what is written in the documentation).
-        // see https://www.freedesktop.org/software/systemd/man/sd_journal_print.html
-        _cleanup_free_ char *file_info = NULL;
-        _cleanup_free_ char *line_info = NULL;
-        asprintf(&file_info, "CODE_FILE=%s", file);
-        asprintf(&line_info, "CODE_LINE=%s", line);
-
-        // clang-format off
-        sd_journal_send_with_location(
-                file_info, line_info, func,
-                "HIRTE_LOG_LEVEL=%s", log_level_to_string(lvl),
-                "HIRTE_MESSAGE=%s", msg,
-                "HIRTE_DATA=%s", data,
-                NULL);
-        // clang-format on
-}
-
+                const char *data);
 void hirte_log_to_stdout_with_location(
                 LogLevel lvl,
                 const char *file,
                 const char *line,
                 const char *func,
                 const char *msg,
-                const char *data) {
-
-        time_t t = time(NULL);
-        const size_t timestamp_size = 9;
-        char timebuf[timestamp_size];
-        timebuf[strftime(timebuf, sizeof(timebuf), "%H:%M:%S", localtime(&t))] = '\0';
-
-        // clang-format off
-        fprintf(stderr, "%s %s\t%s:%s %s\t{msg: %s, data: %s}\n", 
-                timebuf, log_level_to_string(lvl), 
-                file, line, func,
-                msg, data);
-        // clang-format on
-}
+                const char *data);
 
 #define hirte_log_to_stdout hirte_log_to_stdout_with_location
 #define hirte_log_to_journald hirte_log_to_journald_with_location
 
-// NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
-static struct {
+struct HirteLogConfig {
         LogFn log_fn;
         LogLevel level;
         bool is_quiet;
-} HirteLogConfig;
-// NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
+};
+extern struct HirteLogConfig config;
 
-void hirte_log_set_log_fn(LogFn log_fn) {
-        HirteLogConfig.log_fn = log_fn;
-}
+void hirte_log_set_log_fn(LogFn log_fn);
+void hirte_log_set_level(LogLevel level);
+void hirte_log_set_quiet(bool is_quiet);
+bool shouldLog(LogLevel lvl);
 
-void hirte_log_set_level(LogLevel level) {
-        HirteLogConfig.level = level;
-}
-
-void hirte_log_set_quiet(bool is_quiet) {
-        HirteLogConfig.is_quiet = is_quiet;
-}
-
-bool shouldLog(LogLevel lvl) {
-        return (HirteLogConfig.level <= lvl && !HirteLogConfig.is_quiet && HirteLogConfig.log_fn != NULL);
-}
-
-
-#define hirte_log(lvl, msg, ...)                                                                  \
-        if (shouldLog(lvl)) {                                                                     \
-                HirteLogConfig.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, ""); \
+#define hirte_log(lvl, msg, ...)                                                          \
+        if (shouldLog(lvl)) {                                                             \
+                config.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, ""); \
         }
 
 #define hirte_log_debug(msg) hirte_log(LOG_LEVEL_DEBUG, msg)
@@ -116,11 +66,11 @@ bool shouldLog(LogLevel lvl) {
 #define hirte_log_error(msg) hirte_log(LOG_LEVEL_ERROR, msg)
 
 
-#define hirte_logf(lvl, msgfmt, ...)                                                              \
-        if (shouldLog(lvl)) {                                                                     \
-                _cleanup_free_ char *msg = NULL;                                                  \
-                asprintf(&msg, msgfmt, __VA_ARGS__);                                              \
-                HirteLogConfig.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, ""); \
+#define hirte_logf(lvl, msgfmt, ...)                                                      \
+        if (shouldLog(lvl)) {                                                             \
+                _cleanup_free_ char *msg = NULL;                                          \
+                asprintf(&msg, msgfmt, __VA_ARGS__);                                      \
+                config.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, ""); \
         }
 
 #define hirte_log_debugf(fmt, ...) hirte_logf(LOG_LEVEL_DEBUG, fmt, __VA_ARGS__)
@@ -129,11 +79,11 @@ bool shouldLog(LogLevel lvl) {
 #define hirte_log_errorf(fmt, ...) hirte_logf(LOG_LEVEL_ERROR, fmt, __VA_ARGS__)
 
 
-#define hirte_log_with_data(lvl, msg, datafmt, ...)                                                 \
-        if (shouldLog(lvl)) {                                                                       \
-                _cleanup_free_ char *data = NULL;                                                   \
-                asprintf(&data, datafmt, __VA_ARGS__);                                              \
-                HirteLogConfig.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, data); \
+#define hirte_log_with_data(lvl, msg, datafmt, ...)                                         \
+        if (shouldLog(lvl)) {                                                               \
+                _cleanup_free_ char *data = NULL;                                           \
+                asprintf(&data, datafmt, __VA_ARGS__);                                      \
+                config.log_fn(lvl, __FILE__, _SD_STRINGIFY(__LINE__), __func__, msg, data); \
         }
 
 #define hirte_log_debug_with_data(msg, datafmt, ...) \
