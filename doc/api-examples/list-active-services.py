da73b4e31993c3aed6d7cff144a0012529141b4a
Alexander Larsson <alexl@redhat.com>
2023-02-07 10:47:12 +0100

    Add some simple API use examples
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

---
+++ doc/api-examples/list-active-services.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from collections import namedtuple
+import dasbus.connection
+bus = dasbus.connection.SessionMessageBus()
+
+NodeUnitInfo = namedtuple("NodeUnitInfo", ["node", "name", "description", "load_state", "active_state", "sub_state", "follower", "object_path", "job_id", "job_type", "job_object_path"])
+
+manager = bus.get_proxy("org.containers.hirte",  "/org/containers/hirte")
+units = manager.ListUnits()
+for u in units:
+    info = NodeUnitInfo(*u)
+    if info.active_state == "active" and info.name.endswith(".service"):
+        print(f"Node: {info.node}, Unit: {info.name}")
