5d9f5a770ff9a4cb75d7c34a1bd9fe7861ecf2d3
Mark Kemel <mkemel@redhat.com>
2023-02-13 21:48:09 +0200

    hirtectl: basic table for list-units output
    Added a basic, 100-characters-wide table for list-units output.
    
    Signed-off-by: Mark Kemel <mkemel@redhat.com>

--- src/client/printer.c
+++ src/client/printer.c
@@ -7,20 +7,27 @@
 #include "printer.h"
 #include "types.h"
 
+/* Prints 100-characters-wide table */
 void print_unit_list_simple(UnitList *unit_list) {
         UnitInfo *unit = NULL;
+        printf("%-80.80s|%9s|%9s\n", "ID", "ACTIVE", "SUB");
+        printf("====================================================================================================\n");
         LIST_FOREACH(units, unit, unit_list->units) {
-                printf("%s\n", unit->id);
+                printf("%-80.80s|%9s|%9s\n", unit->id, unit->active_state, unit->sub_state);
         }
 }
 
+/* Prints 100-characters-wide table */
 void print_nodes_unit_list_simple(UnitList *unit_list) {
         UnitInfo *unit = NULL;
+        printf("%-20.20s|%-59.59s|%9s|%9s\n", "NODE", "ID", "ACTIVE", "SUB");
+        printf("====================================================================================================\n");
         LIST_FOREACH(units, unit, unit_list->units) {
-                printf("%s: %s\n", unit->node, unit->id);
+                printf("%-20.20s|%-59.59s|%9s|%9s\n", unit->node, unit->id, unit->active_state, unit->sub_state);
         }
 }
 
+/* We can implement here other types of printers, e.g. json printer */
 Printer get_simple_printer() {
         Printer p = { .print_unit_list = &print_unit_list_simple,
                       .print_nodes_unit_list = &print_nodes_unit_list_simple };
