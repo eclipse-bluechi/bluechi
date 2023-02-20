2eb194bb90e2d8fefb454bb3c2b69e00df02a34b
Martin Perina <mperina@redhat.com>
2023-02-20 18:25:05 +0100

    Introduce common configuration infrastructure
    Introduces common configuration infracture, which provides:
    
    * initialization and disposal of a global configuration
    * loading of configuration from a specified configuration file
    * setting and getting a value of a specified configuration option
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ src/libhirte/common/config.c
@@ -0,0 +1,259 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <asm-generic/errno-base.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "libhirte/hashmap/hashmap.h"
+#include "libhirte/ini/ini.h"
+
+#include "common.h"
+#include "config.h"
+
+/*
+ * Structure holding the configuration is just hiding that it uses a hashmap structure internally
+ */
+struct config {
+        struct hashmap *cfg_store;
+        char *default_section;
+};
+
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+static int option_compare(const void *a, const void *b, void *udata) { // NOLINT
+        const struct config_option *option_a = a;
+        const struct config_option *option_b = b;
+        return !(streq(option_a->section, option_b->section) && streq(option_a->name, option_b->name));
+}
+#pragma GCC diagnostic pop
+
+static uint64_t option_hash(const void *item, uint64_t seed0, uint64_t seed1) {
+        const struct config_option *option_a = item;
+        char *sect_and_name = NULL;
+        // there is no way how to tell hashmap implementation that there was an error during hash generation
+        assert(asprintf(&sect_and_name, "%s=%s", option_a->section, option_a->name) != -1);
+        uint64_t hash = hashmap_sip(sect_and_name, strlen(sect_and_name), seed0, seed1);
+        free(sect_and_name);
+        return hash;
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+/*
+ * Handle used to store option parsed from a file into a configuration.
+ *
+ * Returns 1 on success, 0 on error as per ini library requirements.
+ */
+static int parsing_handler(void *user, const char *section, const char *name, const char *value) { // NOLINT
+        if (user == NULL) {
+                return 0;
+        }
+        struct config *config = (struct config *) user;
+        if (cfg_s_set_value(config, section, name, value) != 0) {
+                return 0;
+        }
+        return 1;
+}
+#pragma GCC diagnostic pop
+
+int cfg_initialize(struct config **config) {
+        struct config *new_cfg = malloc(sizeof(struct config));
+        if (new_cfg == NULL) {
+                // error during configuration structure initialization
+                return -ENOMEM;
+        }
+        new_cfg->cfg_store = hashmap_new(
+                        sizeof(struct config_option), 0, 0, 0, option_hash, option_compare, NULL, NULL);
+        if (new_cfg->cfg_store == NULL) {
+                // error during hashmap initialization
+                free(new_cfg);
+                return -ENOMEM;
+        }
+        char *section_copy = strdup(CFG_SECT_GLOBAL);
+        if (section_copy == NULL) {
+                // error during default section initialization
+                hashmap_free(new_cfg->cfg_store);
+                free(new_cfg);
+                return -ENOMEM;
+        }
+        new_cfg->default_section = section_copy;
+
+        *config = new_cfg;
+        return 0;
+}
+
+void cfg_dispose(struct config *config) {
+        hashmap_free(config->cfg_store);
+        free(config->default_section);
+        free(config);
+}
+
+int cfg_load_complete_configuration(
+                struct config *config,
+                const char *default_config_file,
+                const char *custom_config_file,
+                const char *custom_config_directory) {
+        int result = 0;
+
+        if (default_config_file != NULL) {
+                result = cfg_load_from_file(config, default_config_file);
+                if (result != 0) {
+                        return result;
+                }
+        }
+
+        if (custom_config_file != NULL) {
+                result = cfg_load_from_file(config, custom_config_file);
+                // if custom config file doesn't exist, continue
+                if (result != 0 && result != -ENOENT) {
+                        return result;
+                }
+        }
+
+        if (custom_config_directory != NULL) {
+                // TODO: https://github.com/containers/hirte/issues/148
+        }
+
+        result = cfg_load_from_env(config);
+
+        return result;
+}
+
+int cfg_load_from_file(struct config *config, const char *config_file) {
+        if (config_file == NULL) {
+                return -EINVAL;
+        }
+        if (access(config_file, F_OK) != 0) {
+                return -ENOENT;
+        }
+        return ini_parse(config_file, parsing_handler, config);
+}
+
+int cfg_load_from_env(struct config *config) {
+        // env_vars and option_names need to match!
+        const char *env_vars[] = { "HIRTE_LOG_LEVEL", "HIRTE_LOG_TARGET", "HIRTE_LOG_IS_QUIET" };
+        const char *option_names[] = { CFG_LOG_LEVEL, CFG_LOG_TARGET, CFG_LOG_IS_QUIET };
+
+        int length = sizeof(env_vars) / sizeof(env_vars[0]);
+        for (int i = 0; i < length; i++) {
+                char *value = getenv(env_vars[i]);
+                if (value != NULL && strlen(value) > 0) {
+                        // environment variable is defined and has a non-empty value, so store it to the configuration
+                        int result = cfg_set_value(config, option_names[i], value);
+                        if (result != 0) {
+                                return result;
+                        }
+                }
+        }
+        return 0;
+}
+
+const char *cfg_get_default_section(struct config *config) {
+        return config->default_section;
+}
+
+int cfg_set_default_section(struct config *config, const char *section_name) {
+        if (config == NULL) {
+                return -EINVAL;
+        }
+        if (section_name == NULL) {
+                return -EINVAL;
+        }
+        char *section_copy = strdup(section_name);
+        if (section_copy == NULL) {
+                return -ENOMEM;
+        }
+        free(config->default_section);
+        config->default_section = section_copy;
+
+        return 0;
+}
+
+int cfg_set_value(struct config *config, const char *option_name, const char *option_value) {
+        return cfg_s_set_value(config, config->default_section, option_name, option_value);
+}
+
+int cfg_s_set_value(
+                struct config *config,
+                const char *section_name,
+                const char *option_name,
+                const char *option_value) {
+        char *section_copy = strdup(section_name);
+        if (section_copy == NULL) {
+                return -ENOMEM;
+        }
+
+        char *name_copy = strdup(option_name);
+        if (name_copy == NULL) {
+                free(section_copy);
+                return -ENOMEM;
+        }
+
+        char *value_copy = NULL;
+        if (option_value != NULL && strlen(option_value) > 0) {
+                // storing non NULL value, so copy is needed
+                value_copy = strdup(option_value);
+                if (value_copy == NULL) {
+                        free(section_copy);
+                        free(name_copy);
+                        return -ENOMEM;
+                }
+        }
+        struct config_option *replaced = hashmap_set(
+                        config->cfg_store,
+                        &(struct config_option){
+                                        .section = section_copy, .name = name_copy, .value = value_copy });
+        if (hashmap_oom(config->cfg_store)) {
+                // OOM during hashmap set operation
+                free(section_copy);
+                free(name_copy);
+                free(value_copy);
+                return -ENOMEM;
+        }
+
+        if (replaced != NULL) {
+                // option has been replaced with the new one, we need to deallocate the old one
+                free(replaced->section);
+                free(replaced->name);
+                free(replaced->value);
+        }
+        return 0;
+}
+
+const char *cfg_get_value(struct config *config, const char *option_name) {
+        return cfg_s_get_value(config, config->default_section, option_name);
+}
+
+const char *cfg_s_get_value(struct config *config, const char *section_name, const char *option_name) {
+        struct config_option *found = (struct config_option *) hashmap_get(
+                        config->cfg_store,
+                        &(struct config_option){ .section = (char *) section_name,
+                                                 .name = (char *) option_name });
+        if (found != NULL) {
+                return found->value;
+        }
+
+        return NULL;
+}
+
+bool cfg_get_bool_value(struct config *config, const char *option_name) {
+        return cfg_s_get_bool_value(config, config->default_section, option_name);
+}
+
+bool cfg_s_get_bool_value(struct config *config, const char *section_name, const char *option_name) {
+        bool result = false;
+        const char *value = cfg_s_get_value(config, section_name, option_name);
+        if (value != NULL) {
+                result = streqi("true", value) || streqi("t", value) || streqi("yes", value) ||
+                                streqi("y", value) || streqi("on", value);
+        }
+        return result;
+}
+
+void config_iterate(struct config *config, bool (*iter)(const void *item, void *udata), void *udata) {
+        hashmap_scan(config->cfg_store, iter, udata);
+}
---
+++ src/libhirte/test/config_test.c
@@ -0,0 +1,342 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "libhirte/common/config.h"
+
+void _config_set_and_get(
+                struct config *config,
+                const char *option,
+                const char *value,
+                const char *expected_value,
+                bool null_value) {
+        assert(option != NULL);
+
+        assert(cfg_set_value(config, option, value) == 0);
+
+        const char *found_value = cfg_get_value(config, option);
+
+        // found value should not be NULL unless we stored an option with NULL value
+        assert(null_value || found_value != NULL);
+
+        assert(null_value || strcmp(value, expected_value) == 0);
+}
+
+void test_config_set_get() {
+    struct config *config = NULL;
+    cfg_initialize(&config);
+
+    _config_set_and_get(config, "key1", "value1", "value1", false);
+    _config_set_and_get(config, "key2", NULL, NULL, true);
+    _config_set_and_get(config, "key1", "value3", "value3", false);
+    _config_set_and_get(config, "key2", "value4", "value4", false);
+
+    cfg_dispose(config);
+}
+
+void _config_set_get_bool(
+                    struct config *config,
+                    const char *option,
+                    const char *value,
+                    bool expected_value) {
+        assert(option != NULL);
+
+        assert(cfg_set_value(config, option, value) == 0);
+
+        assert(cfg_get_bool_value(config, option) == expected_value);
+}
+
+void test_config_set_get_bool() {
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        _config_set_get_bool(config, "key01", "true", true);
+        _config_set_get_bool(config, "key02", "True", true);
+        _config_set_get_bool(config, "key03", "yes", true);
+        _config_set_get_bool(config, "key04", "YeS", true);
+        _config_set_get_bool(config, "key05", "on", true);
+        _config_set_get_bool(config, "key06", "ON", true);
+        _config_set_get_bool(config, "key07", NULL, false);
+        _config_set_get_bool(config, "key08", "bla", false);
+        _config_set_get_bool(config, "key07", "false", false);
+        _config_set_get_bool(config, "key07", "FALSE", false);
+        _config_set_get_bool(config, "key07", "nO", false);
+        _config_set_get_bool(config, "key07", "No", false);
+        _config_set_get_bool(config, "key07", "OfF", false);
+        _config_set_get_bool(config, "key07", "off", false);
+
+        cfg_dispose(config);
+}
+
+void _config_section_set_and_get(
+                struct config *config,
+                const char *section,
+                const char *option,
+                const char *value,
+                const char *expected_value,
+                bool null_value) {
+        assert(option != NULL);
+
+        assert(cfg_s_set_value(config, section, option, value) == 0);
+
+        const char *found_value = cfg_s_get_value(config, section, option);
+
+        // found value should not be NULL unless we stored an option with NULL value
+        assert(null_value || found_value != NULL);
+
+        assert(null_value || strcmp(value, expected_value) == 0);
+}
+
+void test_config_section_set_get() {
+        const char *KEY = "key";
+        const char *S2 = "s_2";
+        const char *S3 = "s_3";
+        const char *KEY_ORIG_VALUE = "v_1";
+        const char *KEY_NEW_VALUE = "new_v_1";
+        const char *KEY_S2_ORIG_VALUE = "v_2";
+        const char *KEY_S2_NEW_VALUE = "new_v_2";
+        const char *KEY_S3_ORIG_VALUE = "v_3";
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        _config_set_and_get(config, KEY, KEY_ORIG_VALUE, KEY_ORIG_VALUE, false);
+
+        // add the same key into section 2 and the value of the key in global section shouldn't change
+        _config_section_set_and_get(config, KEY, S2, KEY_S2_ORIG_VALUE, KEY_S2_ORIG_VALUE, false);
+        assert(strcmp(cfg_get_value(config, KEY), KEY_ORIG_VALUE) == 0);
+
+        // add the same key into section 3 and the value of the key in global section and in section 2 shouldn't change
+        _config_section_set_and_get(config, KEY, S3, KEY_S3_ORIG_VALUE, KEY_S3_ORIG_VALUE, false);
+        assert(strcmp(cfg_get_value(config, KEY), KEY_ORIG_VALUE) == 0);
+        assert(strcmp(cfg_s_get_value(config, KEY, S2), KEY_S2_ORIG_VALUE) == 0);
+
+        // change the value of the key in global section and the value of the key in section 2 and 3 shouldn't change
+        _config_set_and_get(config, KEY, KEY_NEW_VALUE, KEY_NEW_VALUE, false);
+        assert(strcmp(cfg_s_get_value(config, KEY, S2), KEY_S2_ORIG_VALUE) == 0);
+        assert(strcmp(cfg_s_get_value(config, KEY, S3), KEY_S3_ORIG_VALUE) == 0);
+
+        // change the value of the key in section 2 and the value of the key in global section and section 3 shouldn't change
+        _config_section_set_and_get(config, KEY, S2, KEY_S2_NEW_VALUE, KEY_S2_NEW_VALUE, false);
+        assert(strcmp(cfg_get_value(config, KEY), KEY_NEW_VALUE) == 0);
+        assert(strcmp(cfg_s_get_value(config, KEY, S3), KEY_S3_ORIG_VALUE) == 0);
+
+        cfg_dispose(config);
+}
+
+void test_non_existent_option() {
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        assert(cfg_get_value(config, "non existent key") == NULL);
+
+        cfg_dispose(config);
+}
+
+void test_parse_config_from_file() {
+        const char *cfg_file_content =
+                "key1 = value1\n"
+                "key2 = value 2\n"
+                "# key3 = ignored\n"
+                "key4 = value 4\n"
+                "key5=\n";
+
+
+        // check if tmp file was created and filled-in successfully
+        char cfg_file_name[30];
+        sprintf(cfg_file_name, "/tmp/cfg-file-%6d", rand() % 999999);
+        FILE *cfg_file = fopen(cfg_file_name, "we");
+        assert(cfg_file != NULL);
+        fputs(cfg_file_content, cfg_file);
+        fclose(cfg_file);
+
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        int result = cfg_load_from_file(config, cfg_file_name);
+        assert(result == 0);
+
+        assert(cfg_get_value(config, "key1") != NULL);
+        assert(strcmp(cfg_get_value(config, "key1"), "value1") == 0);
+        assert(cfg_get_value(config, "key2") != NULL);
+        assert(strcmp(cfg_get_value(config, "key2"), "value 2") == 0);
+        assert(cfg_get_value(config, "key3") == NULL);
+        assert(cfg_get_value(config, "key4") != NULL);
+        assert(strcmp(cfg_get_value(config, "key4"), "value 4") == 0);
+        assert(cfg_get_value(config, "key5") == NULL);
+
+        cfg_dispose(config);
+
+        unlink(cfg_file_name);
+}
+
+void test_parse_sectioned_config_from_file() {
+        const char *cfg_file_content =
+                "key = value1\n"
+                "[section2]\n"
+                "key = value 2\n"
+                "# key = ignored\n"
+                "[section 3]\n"
+                "# [ignored section]\n"
+                "key = value 3\n";
+
+
+        // check if tmp file was created and filled-in successfully
+        char cfg_file_name[35];
+        sprintf(cfg_file_name, "/tmp/sect-cfg-file-%6d", rand() % 999999);
+        FILE *cfg_file = fopen(cfg_file_name, "we");
+        assert(cfg_file != NULL);
+        fputs(cfg_file_content, cfg_file);
+        fclose(cfg_file);
+
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        int result = cfg_load_from_file(config, cfg_file_name);
+        assert(result == 0);
+
+        // key value from the global section
+        assert(cfg_get_value(config, "key") != NULL);
+        assert(strcmp(cfg_get_value(config, "key"), "value1") == 0);
+
+        // key value from section2
+        assert(cfg_s_get_value(config, "section2", "key") != NULL);
+        assert(strcmp(cfg_s_get_value(config, "section2", "key"), "value 2") == 0);
+
+        // key value from section 3
+        assert(cfg_s_get_value(config, "section 3", "key") != NULL);
+        assert(strcmp(cfg_s_get_value(config, "section 3", "key"), "value 3") == 0);
+
+        cfg_dispose(config);
+
+        unlink(cfg_file_name);
+}
+
+void dispose_env() {
+        unsetenv("HIRTE_LOG_LEVEL");
+        unsetenv("HIRTE_LOG_TARGET");
+        unsetenv("HIRTE_LOG_IS_QUIET");
+}
+
+void test_default_section() {
+        const char *NEW_DEFAULT_SECTION = "my_section";
+        const char *KEY = "key1";
+        const char *OLD_SECTION_VALUE1 = "value1";
+        const char *NEW_SECTION_VALUE = "value2";
+        const char *OLD_SECTION_VALUE2 = "value3";
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        char *old_default_section = strdup(cfg_get_default_section(config));
+
+        // Test setting and getting value using old_default_section
+        _config_set_and_get(config, KEY, OLD_SECTION_VALUE1, OLD_SECTION_VALUE1, false);
+
+        cfg_set_default_section(config, NEW_DEFAULT_SECTION);
+
+        // Test setting and getting value using NEW_DEFAULT_SECTION
+        _config_set_and_get(config, KEY, NEW_SECTION_VALUE, NEW_SECTION_VALUE, false);
+
+        // Check that values in old_default_section didn't change
+        assert(strcmp(cfg_s_get_value(config, old_default_section, KEY), OLD_SECTION_VALUE1) == 0);
+
+        // Change value in old_default_section and check the value didn't change in NEW_DEFAULT_SECTION
+        _config_section_set_and_get(config, old_default_section, KEY, OLD_SECTION_VALUE2, OLD_SECTION_VALUE2, false);
+        assert(strcmp(cfg_s_get_value(config, NEW_DEFAULT_SECTION, KEY), NEW_SECTION_VALUE) == 0);
+
+        free(old_default_section);
+        cfg_dispose(config);
+}
+
+void test_parse_config_from_env() {
+        const char* LOG_LEVEL_DEBUG = "DEBUG";
+        const char* LOG_TARGET_STDERR = "stderr";
+        const char* LOG_IS_QUIET_OFF = "false";
+
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        // initialize environment
+        setenv("HIRTE_LOG_LEVEL", LOG_LEVEL_DEBUG, 1);
+        setenv("HIRTE_LOG_TARGET", LOG_TARGET_STDERR, 1);
+        setenv("HIRTE_LOG_IS_QUIET", LOG_IS_QUIET_OFF, 1);
+
+        // parse config from environment
+        cfg_load_from_env(config);
+
+        const char *value = NULL;
+
+        value = cfg_get_value(config, CFG_LOG_LEVEL);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_LEVEL_DEBUG) == 0);
+
+        value = cfg_get_value(config, CFG_LOG_TARGET);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_TARGET_STDERR) == 0);
+
+        value = cfg_get_value(config, CFG_LOG_IS_QUIET);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_IS_QUIET_OFF) == 0);
+
+        cfg_dispose(config);
+        dispose_env();
+}
+
+void test_env_override_config() {
+        const char* LOG_LEVEL_DEBUG = "DEBUG";
+        const char* LOG_LEVEL_INFO = "INFO";
+        const char* LOG_TARGET_STDERR = "stderr";
+        const char* LOG_IS_QUIET_ON = "on";
+
+        struct config *config = NULL;
+        cfg_initialize(&config);
+
+        // initialize default configuration
+        cfg_set_value(config, CFG_LOG_LEVEL, LOG_LEVEL_DEBUG);
+        cfg_set_value(config, CFG_LOG_TARGET, LOG_TARGET_STDERR);
+        cfg_set_value(config, CFG_LOG_IS_QUIET, LOG_IS_QUIET_ON);
+
+        // initialize environment
+        // log level should be overridden
+        setenv("HIRTE_LOG_LEVEL", LOG_LEVEL_INFO, 1);
+        // empty string in shell is considered an empty value, so log target should not be overridden
+        setenv("HIRTE_LOG_TARGET", "", 1);
+        // log quiet is not defined in env, so it should not be overridden
+
+        // parse config from environment
+        cfg_load_from_env(config);
+
+        const char *value = NULL;
+
+        value = cfg_get_value(config, CFG_LOG_LEVEL);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_LEVEL_INFO) == 0);
+
+        value = cfg_get_value(config, CFG_LOG_TARGET);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_TARGET_STDERR) == 0);
+
+        value = cfg_get_value(config, CFG_LOG_IS_QUIET);
+        assert(value != NULL);
+        assert(strcmp(value, LOG_IS_QUIET_ON) == 0);
+
+        cfg_dispose(config);
+        dispose_env();
+}
+
+int main() {
+        test_config_set_get();
+        test_config_set_get_bool();
+        test_config_section_set_get();
+        test_non_existent_option();
+        test_parse_config_from_file();
+        test_parse_sectioned_config_from_file();
+        test_default_section();
+        test_parse_config_from_env();
+        test_env_override_config();
+        return EXIT_SUCCESS;
+}
