5704d6335aff04b73b9ec55df39361bb0acb8906
Martin Perina <mperina@redhat.com>
2023-02-20 17:48:22 +0100

    Use constants for option names
    Introduce `libhirte/common/config.h` which should be the main focal
    point for application configuration. In this patch only option names are
    defined there and reused in all places, where configuration is being
    parsed.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ src/libhirte/common/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/*
+ * Existing configuration options
+ */
+#define CFG_LOG_LEVEL "Level"
+#define CFG_LOG_TARGET "Target"
+#define CFG_LOG_IS_QUIET "Quiet"
+#define CFG_MANAGER_HOST "Host"
+#define CFG_MANAGER_PORT "Port"
+#define CFG_NODE_NAME "Name"
+#define CFG_ALLOWED_NODE_NAMES "Nodes"
