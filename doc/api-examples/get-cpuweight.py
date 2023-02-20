46e1480e8f39f5f9a351eade34c4758262a6eac5
Alexander Larsson <alexl@redhat.com>
2023-02-20 12:37:01 +0100

    Add property setter and getter for single property
    This also updates the API examples to demonstrate this.
    
    Signed-off-by: Alexander Larsson <alexl@redhat.com>

--- doc/api-examples/get-cpuweight.py
+++ doc/api-examples/get-cpuweight.py
@@ -2,21 +2,20 @@
 
 import sys
 from dasbus.typing import get_native
+from dasbus.typing import Variant
 import dasbus.connection
 bus = dasbus.connection.SessionMessageBus()
 
-if len(sys.argv) != 5:
-    print("No node name, unit, interface and property supplied")
+if len(sys.argv) != 3:
+    print("No node name, unit supplied")
     sys.exit(1)
 
 node_name = sys.argv[1]
 unit_name = sys.argv[2]
-iface_name = sys.argv[3]
-property_name = sys.argv[4]
 
 manager = bus.get_proxy("org.containers.hirte",  "/org/containers/hirte")
 node_path = manager.GetNode(node_name)
 node = bus.get_proxy("org.containers.hirte",  node_path)
 
-val = node.GetUnitProperty(unit_name, iface_name, property_name)
-print(f"{property_name}: {get_native(val)}")
+value = node.GetUnitProperty(unit_name, "org.freedesktop.systemd1.Service", "CPUWeight")
+print(value)
