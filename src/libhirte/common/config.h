9b958b1fd905ec037986032af7d517cb18258075
Martin Perina <mperina@redhat.com>
2023-02-22 13:51:47 +0100

    Rename config option Port to ManagerPort
    Renames config option Port to ManagerPort to make this option more clear
    in configuration files.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- src/libhirte/common/config.h
+++ src/libhirte/common/config.h
@@ -12,7 +12,7 @@
 #define CFG_LOG_TARGET "Target"
 #define CFG_LOG_IS_QUIET "Quiet"
 #define CFG_MANAGER_HOST "ManagerHost"
-#define CFG_MANAGER_PORT "Port"
+#define CFG_MANAGER_PORT "ManagerPort"
 #define CFG_NODE_NAME "NodeName"
 #define CFG_ALLOWED_NODE_NAMES "Nodes"
 
