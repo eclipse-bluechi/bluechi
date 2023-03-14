285d79aa992dcfec0def67260881d2b5b85a34a4
Michael Engel <mengel@redhat.com>
2023-03-14 12:12:03 +0100

    set RELEASE in srpm build script
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- build-scripts/build-srpm.sh
+++ build-scripts/build-srpm.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -xe
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+# Mark current directory as safe for git to be able to parse git hash
+git config --global --add safe.directory $(pwd)
+
+# Package release
+#
+# Use following for official releases
+#RELEASE="1"
+# Use following for nightly builds
+RELEASE="0.$(date +%04Y%02m%02d%02H%02M).git$(git rev-parse --short ${GITHUB_SHA:-HEAD})"
+
+# Set version and release
+sed \
+    -e "s|@RELEASE@|${RELEASE}|g" \
+    < $1 \
+    > hirte.spec
+
 # Prepare source archive
 [[ -d rpmbuild/SOURCES ]] || mkdir -p rpmbuild/SOURCES
-git archive --format=tar --add-file=$1 HEAD | gzip -9 > rpmbuild/SOURCES/hirte-$VERSION.tar.gz
+git archive --format=tar --add-file=hirte.spec HEAD | gzip -9 > rpmbuild/SOURCES/hirte-$VERSION.tar.gz
 
 # Build source package
 rpmbuild \
     -D "_topdir rpmbuild" \
-    -bs $1
+    -bs hirte.spec
