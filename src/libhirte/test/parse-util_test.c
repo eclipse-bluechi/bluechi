7ce680a1b5687b1d7a17014825ddac1368a3a4d5
Michael Engel <mengel@redhat.com>
2023-03-27 14:58:20 +0200

    parse_long only returns value if successful
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- src/libhirte/test/parse-util_test.c
+++ src/libhirte/test/parse-util_test.c
@@ -18,13 +18,13 @@ bool test_parse_long(const char *input, bool expectedReturn, long expectedResult
         int ret = parse_long(input, &res);
         if ((ret != expectedReturn) || (res != expectedResult)) {
                 ret = asprintf(&msg,
-                         "FAILED: %s\n\tInput: '%s'\n\tExpected: Return=%d, Result=%ld\n\tGot: Return=%d, Result=%ld\n",
-                         __FUNCTION_NAME__,
-                         input,
-                         expectedReturn,
-                         expectedResult,
-                         ret,
-                         res);
+                               "FAILED: %s\n\tInput: '%s'\n\tExpected: Return=%d, Result=%ld\n\tGot: Return=%d, Result=%ld\n",
+                               __FUNCTION_NAME__,
+                               input,
+                               expectedReturn,
+                               expectedResult,
+                               ret,
+                               res);
         }
 
         if (msg != NULL) {
@@ -41,13 +41,13 @@ bool test_parse_port(const char *input, bool expected_return, uint16_t expected_
         bool ret = parse_port(input, &res);
         if ((ret != expected_return) || (res != expected_result)) {
                 ret = asprintf(&msg,
-                         "FAILED: %s\n\tInput: '%s'\n\tExpected: Return=%d, Result=%d\n\tGot: Return=%d, Result=%d\n",
-                         __FUNCTION_NAME__,
-                         input,
-                         expected_return,
-                         expected_result,
-                         ret,
-                         res);
+                               "FAILED: %s\n\tInput: '%s'\n\tExpected: Return=%d, Result=%d\n\tGot: Return=%d, Result=%d\n",
+                               __FUNCTION_NAME__,
+                               input,
+                               expected_return,
+                               expected_result,
+                               ret,
+                               res);
         }
 
         if (msg != NULL) {
@@ -59,12 +59,15 @@ bool test_parse_port(const char *input, bool expected_return, uint16_t expected_
 
 int main() {
         bool result = true;
+
         result = result && test_parse_long(NULL, false, 0L);
         result = result && test_parse_long("", false, 0L);
         result = result && test_parse_long("foo", false, 0L);
         result = result && test_parse_long("1234", true, 1234L);
-        result = result && test_parse_long("1234abc", false, 1234L);
+        result = result && test_parse_long("1234abc", false, 0L);
         result = result && test_parse_long("abc1234", false, 0L);
+        result = result && test_parse_long("-1234", true, -1234L);
+        result = result && test_parse_long("12.34", false, 0L);
 
         result = result && test_parse_port(NULL, false, 0);
         result = result && test_parse_port("", false, 0);
@@ -74,6 +77,8 @@ int main() {
         result = result && test_parse_port("65535", true, 65535);
         result = result && test_parse_port("65536", false, 0);
         result = result && test_parse_port("-2", false, 0);
+        result = result && test_parse_port("65.536", false, 0);
+
 
         if (!result) {
                 return EXIT_FAILURE;
