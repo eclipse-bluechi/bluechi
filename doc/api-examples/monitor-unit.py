7826af3ebd7fc182258f8769f86341030ce25d52
Martin Perina <mperina@redhat.com>
2023-05-03 15:24:28 +0200

    Fix issues detected by flake8
    Following issue are fixed:
    
    ./doc/api-examples/get-cpuweight.py:4:1: F401 'dasbus.typing.get_native' imported but unused
    ./doc/api-examples/get-cpuweight.py:5:1: F401 'dasbus.typing.Variant' imported but unused
    ./doc/api-examples/monitor-unit.py:4:1: F401 'time' imported but unused
    ./doc/api-examples/monitor-unit.py:9:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/monitor-unit.py:19:1: E305 expected 2 blank lines after class or function definition, found 1
    ./doc/api-examples/monitor-unit.py:24:15: E261 at least two spaces before inline comment
    ./doc/api-examples/monitor-unit.py:38:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/monitor-unit.py:45:25: E201 whitespace after '{'
    ./doc/api-examples/monitor-unit.py:45:51: E703 statement ends with a semicolon
    ./doc/api-examples/monitor-unit.py:47:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/monitor-unit.py:50:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/monitor-unit.py:53:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/monitor-unit.py:58:1: E305 expected 2 blank lines after class or function definition, found 1
    ./doc/api-examples/monitor-unit.py:63:40: E703 statement ends with a semicolon
    ./doc/api-examples/monitor-unit.py:66:68: E703 statement ends with a semicolon
    ./doc/api-examples/monitor-unit.py:68:78: E703 statement ends with a semicolon
    ./doc/api-examples/set-cpuweight.py:4:1: F401 'dasbus.typing.get_native' imported but unused
    ./doc/api-examples/start-unit.py:23:1: E302 expected 2 blank lines, found 1
    ./doc/api-examples/start-unit.py:29:1: E305 expected 2 blank lines after class or function definition, found 1
    ./tests/hirte_tests/provision/containers.py:72:121: E501 line too long (126 > 120 characters)
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- doc/api-examples/monitor-unit.py
+++ doc/api-examples/monitor-unit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import sys
-import time
 from dasbus.typing import get_native
 from dasbus.loop import EventLoop
 import dasbus.connection
+
 
 def print_dict_changes(old, new):
     for key in sorted(set(old.keys()) | set(new.keys())):
@@ -16,12 +16,13 @@ def print_dict_changes(old, new):
             if o != n:
                 print(f" {key}: {o} -> {n}")
 
+
 if len(sys.argv) < 2:
     print("No unit name supplied")
     sys.exit(1)
 
 unit_name = sys.argv[1]
-node_name = "*" # Match all
+node_name = "*"  # Match all
 
 if len(sys.argv) > 2:
     node_name = sys.argv[2]
@@ -35,6 +36,7 @@ monitor = bus.get_proxy("org.containers.hirte",  monitor_path)
 
 old_values = {}
 
+
 def unit_property_changed(node, unit, interface, props):
     old_value = old_values.get(node, {})
     new_value = get_native(props)
@@ -42,30 +44,34 @@ def unit_property_changed(node, unit, interface, props):
     print(f"Unit {unit} on node {node} changed for iface {interface}:")
     print_dict_changes(old_value, new_value)
 
-    old_values[node] = { **old_value, **new_value};
+    old_values[node] = {**old_value, **new_value}
+
 
 def unit_new(node, unit, reason):
     print(f"New Unit {unit} on node {node}, reason: {reason}")
 
+
 def unit_state_changed(node, unit, active_state, substate, reason):
     print(f"Unit {unit} on node {node}, changed to state: {active_state} ({substate}), reason: {reason}")
+
 
 def unit_removed(node, unit, reason):
     print(f"Removed Unit {unit} on node {node}, reason: {reason}")
     if node in old_values:
         del old_values[node]
 
+
 monitor.UnitPropertiesChanged.connect(unit_property_changed)
 monitor.UnitNew.connect(unit_new)
 monitor.UnitStateChanged.connect(unit_state_changed)
 monitor.UnitRemoved.connect(unit_removed)
 
-monitor.Subscribe(node_name, unit_name);
+monitor.Subscribe(node_name, unit_name)
 
 if node_name == "":
-    print(f"Waiting for changes to unit `{unit_name}` on any node");
+    print(f"Waiting for changes to unit `{unit_name}` on any node")
 else:
-    print(f"Waiting for changes to unit `{unit_name}` on node '{node_name}'");
+    print(f"Waiting for changes to unit `{unit_name}` on node '{node_name}'")
 
 loop = EventLoop()
 loop.run()
