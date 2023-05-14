a03d052e01224042fe3813e9c2f2bac708be62e2
Michael Engel <mengel@redhat.com>
2023-05-14 16:12:43 +0200

    fix local networking issue for e2e tests
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- tests/hirte_test/util.py
+++ tests/hirte_test/util.py
@@ -1,7 +1,25 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import socket
+
+
 def read_file(local_file: str) -> (str | None):
     content = None
     with open(local_file, 'r') as fh:
         content = fh.read()
     return content
+
+
+def get_primary_ip() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # connect to an arbitrary address
+        s.connect(("254.254.254.254", 1))
+        ip = s.getsockname()[0]
+    except Exception as ex:
+        print(f"Failed to get IP, falling back to localhost. Exception: {ex}")
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
