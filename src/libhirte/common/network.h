f83f491ff8afebf5851ecb7d57aca6fed567b921
Douglas Schilling Landgraf <dougsland@redhat.com>
2023-06-10 07:05:20 +0000

    network: add dual stack
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/libhirte/common/network.h
+++ src/libhirte/common/network.h
@@ -19,6 +19,7 @@ bool isIPv6Addr(const char *domain);
 int get_address(const char *domain, char **ip_address);
 
 char *typesafe_inet_ntop4(const struct sockaddr_in *addr);
-char *typesafe_inet_ntop6(const struct sockaddr_in *addr);
+char *typesafe_inet_ntop6(const struct sockaddr_in6 *addr);
 
 char *assemble_tcp_address(const struct sockaddr_in *addr);
+char *assemble_tcp_address_v6(const struct sockaddr_in6 *addr);
--- src/libhirte/test/common/network/assemble_tcp_address.c
+++ src/libhirte/test/common/network/assemble_tcp_address.c
@@ -13,6 +13,22 @@
 #        define __FUNCTION_NAME__ __func__
 #endif
 
+int build_sockaddr_in6(const char *host_in, uint16_t port_in, struct sockaddr_in6 *ret) {
+        int r = 0;
+
+        memset(ret, 0, sizeof(*ret));
+        ret->sin6_family = AF_INET6;
+        ret->sin6_port = htons(port_in);
+
+        r = inet_pton(AF_INET6, host_in, &ret->sin6_addr);
+        if (r < 1) {
+                fprintf(stdout, "Invalid host option '%s'\n", host_in);
+                return r;
+        }
+
+        return 0;
+}
+
 int build_sockaddr_in(const char *host_in, uint16_t port_in, struct sockaddr_in *ret) {
         int r = 0;
 
@@ -29,6 +45,27 @@ int build_sockaddr_in(const char *host_in, uint16_t port_in, struct sockaddr_in 
         return 0;
 }
 
+bool test_assemble_tcp_address_v6(const struct sockaddr_in6 *input, const char *expectedResult) {
+        _cleanup_free_ char *result = assemble_tcp_address_v6(input);
+        if ((result == NULL && expectedResult != NULL) || (result != NULL && !streq(result, expectedResult))) {
+                _cleanup_free_ char *msg = NULL;
+                int r = asprintf(
+                                &msg,
+                                "FAILED: %s\n\tExpected: %s\n\tGot: %s\n",
+                                __FUNCTION_NAME__,
+                                expectedResult,
+                                result);
+                if (r < 0) {
+                        fprintf(stderr, "OOM: %s failed", __FUNCTION_NAME__);
+                        return false;
+                }
+                if (msg != NULL) {
+                        fprintf(stderr, "%s", msg);
+                        return false;
+                }
+        }
+        return true;
+}
 
 bool test_assemble_tcp_address(const struct sockaddr_in *input, const char *expectedResult) {
         _cleanup_free_ char *result = assemble_tcp_address(input);
@@ -55,11 +92,15 @@ bool test_assemble_tcp_address(const struct sockaddr_in *input, const char *expe
 int main() {
         bool result = true;
         struct sockaddr_in host;
+        struct sockaddr_in6 host6;
 
         result = result && test_assemble_tcp_address(NULL, NULL);
 
         build_sockaddr_in("127.0.0.1", 808, &host);
         result = result && test_assemble_tcp_address(&host, "tcp:host=127.0.0.1,port=808");
+
+        build_sockaddr_in6("::1", 808, &host6);
+        result = result && test_assemble_tcp_address_v6(&host6, "tcp:host=::1,port=808");
 
         build_sockaddr_in("192.168.178.1", 808, &host);
         result = result && test_assemble_tcp_address(&host, "tcp:host=192.168.178.1,port=808");
