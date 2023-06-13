a6206d70e57ac7fcd93401d516ae3b10f3394419
Douglas Schilling Landgraf <dougsland@redhat.com>
2023-06-13 14:53:37 +0000

    python: setup.py add python 3.11
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -29,5 +29,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
