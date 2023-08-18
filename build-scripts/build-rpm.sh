074df6c61b4c138e6582228969c546ff563b6639
Martin Perina <mwperina@gmail.com>
2023-08-18 12:52:02 +0200

    Make building python3-bluechi conditional (#479)
    * Add missing dependencies to README.developer.md
    
    After some recent changes (for example moving external libraries to
    [subprojects](https://github.com/containers/bluechi/pull/432)) we were
    missing several dependencies required to build the project
    successfully.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>
    
    * Fix RPM package building documentation
    
    1. Adds installation of required packages for RPM build
    2. Updates the part around RPM build customization more clear
    
    Signed-off-by: Martin Perina <mperina@redhat.com>
    
    * Make building python3-bluechi conditional
    
    For some build targets we don't want python3-bluechi to be built, so
    this patch add `--without python` parameter to rpm-build command to
    disable it.
    
    Fixes: https://github.com/containers/bluechi/issues/396
    Signed-off-by: Martin Perina <mperina@redhat.com>
    
    ---------
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- build-scripts/build-rpm.sh
+++ build-scripts/build-rpm.sh
@@ -12,9 +12,10 @@ fi
 rpmbuild \
     --define "_topmdir rpmbuild" \
     --define "_rpmdir rpmbuild" \
+    ${BUILD_RPM_OPTS} \
     --rebuild rpmbuild/SRPMS/*src.rpm
 
 # Move RPMs to exported artifacts
-ARTIFACTS_DIR="${ARTIFACTS_DIR:=$(pwd)/artifacts/rpms/$(date +'%m-%d-%Y')}"
+ARTIFACTS_DIR="${ARTIFACTS_DIR:=$(pwd)/artifacts/rpms-$(date +%04Y%02m%02d%02H%02M)}"
 [[ -d $ARTIFACTS_DIR ]] || mkdir -p $ARTIFACTS_DIR
 find rpmbuild -iname \*rpm | xargs mv -t $ARTIFACTS_DIR
