56256b91ffb180d63f26761b8aafa04b7fa7c440
Martin Perina <mperina@redhat.com>
2023-08-22 10:02:41 +0200

    Introduce support for packit builds
    - Adapt our RPM build scripts to be usable in packit
    - Add support to build RPMs for the most important architectures when PR
      is added/updated
    - Add support to build RPMs for all supported platforms and save produced
      RPMs into our COPR repository
    
    Fixes: https://github.com/containers/bluechi/issues/452
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- build-scripts/build-srpm.sh
+++ build-scripts/build-srpm.sh
@@ -1,38 +1,8 @@
 #!/bin/bash -xe
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-# Parse package version from the project
-meson setup builddir
-VERSION="$(meson introspect --projectinfo builddir | jq -r '.version')"
+source $(dirname "$(readlink -f "$0")")/create-archive.sh
 
-# Mark current directory as safe for git to be able to parse git hash
-if [ "${SKIP_GIT_CONFIG}" != "yes" ]; then
-    git config --global --add safe.directory $(pwd)
-fi
-
-# Package release
-#
-# Use following for official releases
-#RELEASE="1"
-# Use following for nightly builds
-RELEASE="0.$(date +%04Y%02m%02d%02H%02M).git$(git rev-parse --short ${GITHUB_SHA:-HEAD})"
-
-
-# Set version and release
-sed \
-    -e "s|@VERSION@|${VERSION}|g" \
-    -e "s|@RELEASE@|${RELEASE}|g" \
-    < bluechi.spec.in \
-    > bluechi.spec
-
-# Prepare source archive
-[[ -d rpmbuild/SOURCES ]] || mkdir -p rpmbuild/SOURCES
-
-git archive --format=tar --prefix=bluechi-$VERSION/ --add-file=bluechi.spec -o bluechi-$VERSION.tar HEAD
-git submodule foreach --recursive \
-    "git archive --prefix=bluechi-$VERSION/\$path/ --output=\$sha1.tar HEAD && \
-     tar --concatenate --file=$(pwd)/bluechi-$VERSION.tar \$sha1.tar && rm \$sha1.tar"
-gzip bluechi-$VERSION.tar
 mv bluechi-$VERSION.tar.gz rpmbuild/SOURCES/
 
 # Build source package
