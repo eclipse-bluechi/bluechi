216041083a2e850d9fe35c665c7a2b9923b66ef4
minsoo.kim <minsoo.kim@42dot.ai>
2023-08-25 13:35:04 +0900

    Add intergration test for freeze and thaw
    Fixes: #495
    
    Signed-off-by: minsoo.kim <minsoo.kim@42dot.ai>

---
+++ tests/tests/tier0/bluechi-freeze-and-thaw-service/test_bluechi_freeze_and_thaw_service.py
@@ -0,0 +1,62 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import os
+import pytest
+from typing import Dict
+
+from bluechi_test.config import BluechiControllerConfig, BluechiNodeConfig
+from bluechi_test.container import BluechiControllerContainer, BluechiNodeContainer
+from bluechi_test.test import BluechiTest
+
+
+node_foo_name = "node-foo"
+simple_service = "simple.service"
+
+
+def verify_service_start(foo: BluechiNodeContainer):
+    assert foo.wait_for_unit_state_to_be(simple_service, "active")
+
+
+def verify_service_freeze(foo: BluechiNodeContainer):
+    output = foo.get_unit_freezer_state(simple_service)
+    assert output == 'frozen'
+
+
+def verify_service_thaw(foo: BluechiNodeContainer):
+    output = foo.get_unit_freezer_state(simple_service)
+    assert output == 'running'
+
+
+def exec(ctrl: BluechiControllerContainer, nodes: Dict[str, BluechiNodeContainer]):
+    foo = nodes[node_foo_name]
+
+    source_dir = os.path.join(".", "systemd")
+    target_dir = os.path.join("/", "etc", "systemd", "system")
+
+    foo.copy_systemd_service(simple_service, source_dir, target_dir)
+
+    ctrl.start_unit(node_foo_name, simple_service)
+    verify_service_start(foo)
+
+    ctrl.freeze_unit(node_foo_name, simple_service)
+    verify_service_freeze(foo)
+
+    ctrl.thaw_unit(node_foo_name, simple_service)
+    verify_service_thaw(foo)
+
+
+@pytest.mark.timeout(10)
+def test_service_freeze_and_thaw(
+        bluechi_test: BluechiTest,
+        bluechi_ctrl_default_config: BluechiControllerConfig,
+        bluechi_node_default_config: BluechiNodeConfig):
+
+    node_foo_cfg = bluechi_node_default_config.deep_copy()
+    node_foo_cfg.node_name = node_foo_name
+
+    bluechi_ctrl_default_config.allowed_node_names = [node_foo_name]
+
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+    bluechi_test.add_bluechi_node_config(node_foo_cfg)
+
+    bluechi_test.run(exec)
