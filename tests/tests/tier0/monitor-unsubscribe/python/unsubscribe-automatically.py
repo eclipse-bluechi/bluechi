5ffbc24ba522bc5c2b58ef03ef6af0907ecdb547
Michael Engel <mengel@redhat.com>
2023-08-29 09:36:07 +0200

    add integration test for automatic unsubscribe
    Fixes: https://github.com/containers/bluechi/issues/413
    Adds an integration test to verify that unsubscribe is
    automatically called as soon as the consumer stops the
    connection (the event loop, in this case).
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/monitor-unsubscribe/python/unsubscribe-automatically.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from dasbus.loop import EventLoop
+
+from bluechi.api import Manager, Monitor, Node
+
+
+node_name_foo = "node-foo"
+service_simple = "simple.service"
+
+
+class TestUnsubscribe(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.loop = EventLoop()
+        self.mgr = Manager()
+        monitor_path = self.mgr.create_monitor()
+        self.monitor = Monitor(monitor_path=monitor_path)
+
+        self.sub_id = -1
+        self.unsubscribe_ex = None
+
+        def on_unit_removed(node: str, unit: str, reason: str) -> None:
+            # catch exception if raised, otherwise it'll prevent the loop quit
+            # and result in a timeout. assert on the exception later.
+            try:
+                self.monitor.unsubscribe(self.sub_id)
+            except Exception as ex:
+                self.unsubscribe_ex = Exception(f"Failed to unsubscribe '{self.sub_id}', got exception: {ex}")
+            self.loop.quit()
+
+        self.monitor.on_unit_removed(on_unit_removed)
+
+    def test_unsubscribe(self):
+
+        node_foo = Node(node_name_foo)
+
+        # create subscription, start unit and run event loop till removed signal is received
+        self.sub_id = self.monitor.subscribe(node_name_foo, service_simple)
+        assert node_foo.start_unit(service_simple, "replace") != ""
+        self.loop.run()
+
+        assert self.unsubscribe_ex is None
+
+
+if __name__ == "__main__":
+    unittest.main()
