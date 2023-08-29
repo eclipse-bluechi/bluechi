09e1ee01125f296f865395a97dca4cc05ffe749f
Michael Engel <mengel@redhat.com>
2023-08-29 16:11:35 +0200

    added integration test for monitoring specific unit
    Relates to
    https://github.com/containers/bluechi/issues/412
    https://github.com/containers/bluechi/issues/418
    Adds a simple integration test to verify that all signals
    when a unit is started (and stopped) are emitted and a
    monitor to this specific node and unit receives them.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/monitor-specific-node-and-unit/python/monitor.py
@@ -0,0 +1,62 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from dasbus.loop import EventLoop
+
+from bluechi.api import Manager, Monitor, Node, Structure
+
+node_name_foo = "node-foo"
+
+service_simple = "simple.service"
+requesting_simple = "requesting.service"
+
+
+class TestMonitorSpecificNodeAndUnit(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.loop = EventLoop()
+        self.mgr = Manager()
+
+        self.times_new_called = 0
+        self.times_state_changed_called = 0
+        self.times_prop_changed_called = 0
+        self.times_removed_called = 0
+
+    def test_monitor_specific_node_and_unit(self):
+
+        monitor_path = self.mgr.create_monitor()
+        monitor = Monitor(monitor_path=monitor_path)
+
+        def on_unit_new(node: str, unit: str, reason: str) -> None:
+            self.times_new_called += 1
+
+        def on_unit_state_changed(node: str, unit: str, active_state: str, sub_state: str, reason: str) -> None:
+            self.times_state_changed_called += 1
+
+        def on_unit_property_changed(node: str, unit: str, interface: str, props: Structure) -> None:
+            self.times_prop_changed_called += 1
+
+        def on_unit_removed(node: str, unit: str, reason: str) -> None:
+            self.times_removed_called += 1
+            self.loop.quit()
+
+        monitor.on_unit_new(on_unit_new)
+        monitor.on_unit_state_changed(on_unit_state_changed)
+        monitor.on_unit_properties_changed(on_unit_property_changed)
+        monitor.on_unit_removed(on_unit_removed)
+
+        monitor.subscribe(node_name_foo, service_simple)
+
+        node_foo = Node(node_name_foo)
+        assert node_foo.start_unit(service_simple, "replace") != ""
+        self.loop.run()
+
+        assert self.times_new_called >= 1
+        assert self.times_state_changed_called >= 1
+        assert self.times_prop_changed_called >= 1
+        assert self.times_removed_called >= 1
+
+
+if __name__ == "__main__":
+    unittest.main()
