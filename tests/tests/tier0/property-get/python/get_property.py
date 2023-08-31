16e7ca570fec6a7bc48c100c433a6cbd98658530
Michael Engel <mengel@redhat.com>
2023-08-31 15:39:53 +0200

    added integration test for getting a unit property
    Relates to https://github.com/containers/bluechi/issues/401
    Adds an integration test that verifies that a property of
    a systemd unit can be retrieved.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/property-get/python/get_property.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from bluechi.api import Node
+
+
+class TestGetProperty(unittest.TestCase):
+
+    def test_get_property(self):
+        node_foo = Node("node-foo")
+        cpu_weight = node_foo.get_unit_property(
+            "bluechi-agent.service",
+            "org.freedesktop.systemd1.Service",
+            "CPUWeight"
+        )
+
+        assert cpu_weight.get_uint64() > 0
+
+
+if __name__ == "__main__":
+    unittest.main()
