d8980d85b07a83c0353cf9ddaed6c7dffe833a7b
Michael Engel <mengel@redhat.com>
2023-08-31 15:41:29 +0200

    added integration test for getting all properties of a unit
    Relates to https://github.com/containers/bluechi/issues/401
    Adds an integration test that verifies that all properties of
    a systemd unit can be retrieved.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/properties-get/python/get_properties.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from bluechi.api import Node
+
+
+class TestGetProperties(unittest.TestCase):
+
+    def test_get_properties(self):
+        node_foo = Node("node-foo")
+        props = node_foo.get_unit_properties(
+            "bluechi-agent.service",
+            "org.freedesktop.systemd1.Service",
+        )
+
+        assert len(props) > 0
+        # check some properties as example
+        assert props["Type"].get_string() == "simple"
+        assert not props["RemainAfterExit"].get_boolean()
+
+
+if __name__ == "__main__":
+    unittest.main()
