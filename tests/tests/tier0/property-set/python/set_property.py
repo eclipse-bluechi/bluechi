588945f697ced252c9da65d9f6b0387ef02097e6
Michael Engel <mengel@redhat.com>
2023-08-31 15:42:12 +0200

    added integration test for setting a unit property value
    Relates to https://github.com/containers/bluechi/issues/401
    Adds an integration test that verifies that a unit property value
    of a systemd unit can be set.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/property-set/python/set_property.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from bluechi.api import Node, Variant
+
+
+class TestSetProperty(unittest.TestCase):
+
+    def test_set_property(self):
+        node_foo = Node("node-foo")
+
+        unit_description = node_foo.get_unit_property(
+            "simple.service",
+            "org.freedesktop.systemd1.Unit",
+            "Description"
+        )
+        assert unit_description.get_string() == "Just being true once"
+
+        v = Variant("s", "I changed")
+        node_foo.set_unit_properties("simple.service", False, [("Description", v)])
+        new_unit_description = node_foo.get_unit_property(
+            "simple.service",
+            "org.freedesktop.systemd1.Unit",
+            "Description"
+        )
+
+        assert new_unit_description.get_string() == "I changed"
+
+
+if __name__ == "__main__":
+    unittest.main()
