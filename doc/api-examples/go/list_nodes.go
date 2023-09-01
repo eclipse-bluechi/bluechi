c011c6f14da1e9c2cef0159457e530583a0a4ce3
Douglas Schilling Landgraf <dougsland@redhat.com>
2023-09-01 14:23:48 +0000

    doc/examples: golang - list_nodes.go
    Just an example how to use golang and bluechi.
    
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

---
+++ doc/api-examples/go/list_nodes.go
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/godbus/dbus"
+)
+
+// Constants to manage bluechi dbus interface
+const (
+        BcDusInterface = "org.eclipse.bluechi"
+        BcObjectPath = "/org/eclipse/bluechi"
+	MethodListNodes = "org.eclipse.bluechi.Manager.ListNodes"
+)
+
+func main() {
+	var nodes [][]interface{}
+
+	conn, err := dbus.ConnectSystemBus()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect to system bus:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	busObject := conn.Object(BcDusInterface, BcObjectPath)
+	err = busObject.Call(MethodListNodes, 0).Store(&nodes)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to list nodes:", err)
+		os.Exit(1)
+	}
+
+	for _, node := range nodes {
+		fmt.Println("Name:", node[0])
+		fmt.Println("Status:", node[2])
+		fmt.Println("Object Path:", node[1])
+		fmt.Println()
+	}
+}
--- doc/docs/api/examples.md
+++ doc/docs/api/examples.md
@@ -1,5 +1,49 @@
-<!-- markdownlint-disable-file MD013 -->
+<!-- markdownlint-disable-file MD013 MD024 -->
 # Examples using the public D-Bus API
+
+## GoLang
+
+First, make sure the system has golang installed and download the dbus library.
+
+These steps were based on CentOS Stream 9:
+
+### Installing golang
+
+``` bash
+# dnf install go-toolset
+```
+
+### Adding dbus library
+
+``` bash
+# go install github.com/godbus/dbus
+```
+
+### Build the source code
+
+``` bash
+# go build list_nodes.go
+# ./list_nodes
+Name: control
+Status: online
+Object Path: /org/eclipse/bluechi/node/control
+
+Name: node1
+Status: online
+Object Path: /org/eclipse/bluechi/node/node1
+
+Name: qm-node1
+Status: online
+Object Path: /org/eclipse/bluechi/node/qm_2dnode1
+```
+
+### List all nodes
+
+``` go
+--8<-- "api-examples/go/list_nodes.go"
+```
+
+---
 
 ## Python
 
