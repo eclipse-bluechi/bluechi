8626536041ae71ec23981448105f3656116db095
Martin Perina <mperina@redhat.com>
2023-09-30 10:37:31 +0200

    Add description to `build-scripts/install-coverage.sh`
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- build-scripts/install-coverage.sh
+++ build-scripts/install-coverage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+# This script should be executed only from meson as a part of install flow!
+#
+# Install all created `*.gcno` files so they could be packaged into bluechi-coverage RPM.
 COV_DIR="${MESON_INSTALL_DESTDIR_PREFIX}/share/bluechi-coverage"
 mkdir -p "${COV_DIR}"
 find "${MESON_BUILD_ROOT}" -name '*.gcno' -exec cp "{}" "${COV_DIR}" \;
-
