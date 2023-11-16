4fd1a774a41b5f178b37ec23863f1bd3e198d5b4
Michael Engel <mengel@redhat.com>
2023-11-16 09:36:33 +0100

    refactored build and push script for base images
    Split build and push into two distinct functions. Also, renamed
    functions and added an parameter check.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- build-scripts/build-push-containers.sh
+++ build-scripts/build-push-containers.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$( realpath "$0"  )
 SCRIPT_DIR=$(dirname "$SCRIPT_DIR")/
 CONTAINER_FILE_DIR=$SCRIPT_DIR"../tests/containers/"
 
-IMAGE=""
+function push(){
+    buildah manifest push --all $IMAGE "docker://quay.io/bluechi/$IMAGE"
+}
 
-function build_and_push(){
+function build(){
     buildah manifest rm $IMAGE &> /dev/null
     buildah manifest create $IMAGE
 
@@ -18,22 +20,14 @@ function build_and_push(){
     buildah bud --tag "quay.io/bluechi/$IMAGE" \
         --manifest $IMAGE \
         --arch arm64 ${CONTAINER_FILE_DIR}$IMAGE
-
-    if [ "${PUSH_MANIFEST}" == "yes" ]; then
-        buildah manifest push --all $IMAGE "docker://quay.io/bluechi/$IMAGE"
-    fi
 }
 
-function build_base(){
-    IMAGE="build-base"
-    build_and_push
-}
+[ -z $1 ] && echo "Requires image name. Either 'build-base' or 'integration-test-base'." && exit 1
 
-function integration_test_base(){
-    IMAGE="integration-test-base"
-    build_and_push
-}
-
-echo "Building containers and manifest for $1"
-echo "" 
-$1
+echo "Building containers and manifest for '$1'"
+echo ""
+IMAGE="$1"
+build
+if [ "${PUSH_MANIFEST}" == "yes" ]; then
+    push
+fi
