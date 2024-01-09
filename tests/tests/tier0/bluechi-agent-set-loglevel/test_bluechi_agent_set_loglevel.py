8791c8d5460a79c391d2f68a32d3f683c5628d82
Artiom Divak <adivak@redhat.com>
2024-01-09 11:47:34 +0200

    Test for loglevel for controller and agent
    Testing if log level will change if SetLoglevel function will be use
    with bluechictl in container and agent. Loglevel will be set to DEBUG
    and then to INFO. After that the service will be stoped and the journal
    will be checked.
    
    Reletad:https://github.com/eclipse-bluechi/bluechi/issues/668
    Signed-off-by: Artiom Divak <adivak@redhat.com>

---
+++ tests/tests/tier0/bluechi-agent-set-loglevel/test_bluechi_agent_set_loglevel.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from typing import Dict
+from bluechi_test.test import BluechiTest
+from bluechi_test.container import BluechiControllerContainer, BluechiNodeContainer
+from bluechi_test.config import BluechiControllerConfig, BluechiNodeConfig
+
+NODE_FOO = "node-foo"
+
+
+def exec(ctrl: BluechiControllerContainer, nodes: Dict[str, BluechiNodeContainer]):
+    node_foo = nodes[NODE_FOO]
+
+    service = "org.eclipse.bluechi.Agent"
+    object = "/org/eclipse/bluechi"
+    interface = "org.eclipse.bluechi.Agent"
+
+    ctrl.exec_run(f"bluechictl set-loglevel {NODE_FOO} INFO")
+    _, output = node_foo.exec_run(f"busctl  get-property {service} {object} {interface} LogLevel")
+    assert "INFO" in output
+
+    ctrl.exec_run(f"bluechictl set-loglevel {NODE_FOO} DEBUG")
+    _, output = node_foo.exec_run(f"busctl  get-property {service} {object} {interface} LogLevel")
+    assert "DEBUG" in output
+
+
+def test_controller_setloglevel(
+        bluechi_test: BluechiTest,
+        bluechi_node_default_config: BluechiNodeConfig, bluechi_ctrl_default_config: BluechiControllerConfig):
+
+    node_foo_cfg = bluechi_node_default_config.deep_copy()
+    node_foo_cfg.node_name = NODE_FOO
+
+    bluechi_ctrl_default_config.allowed_node_names = [NODE_FOO]
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+
+    bluechi_test.add_bluechi_node_config(node_foo_cfg)
+
+    bluechi_test.run(exec)
---
+++ tests/tests/tier0/bluechi-controller-set-loglevel/test_bluechi_controller_set_loglevel.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from typing import Dict
+from bluechi_test.test import BluechiTest
+from bluechi_test.container import BluechiControllerContainer, BluechiNodeContainer
+from bluechi_test.config import BluechiControllerConfig
+
+
+def exec(ctrl: BluechiControllerContainer, _: Dict[str, BluechiNodeContainer]):
+    service = "org.eclipse.bluechi"
+    object = "/org/eclipse/bluechi"
+    interface = "org.eclipse.bluechi.Controller"
+    ctrl.exec_run("bluechictl set-loglevel INFO")
+    _, output = ctrl.exec_run(f"busctl  get-property {service} {object} {interface} LogLevel")
+    assert "INFO" in output
+    ctrl.exec_run("bluechictl set-loglevel DEBUG")
+    _, output = ctrl.exec_run(f"busctl  get-property {service} {object} {interface} LogLevel")
+    assert "DEBUG" in output
+
+
+def test_controller_setloglevel(
+        bluechi_test: BluechiTest, bluechi_ctrl_default_config: BluechiControllerConfig):
+
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+    bluechi_test.run(exec)
