ac9bc7af4ab17ea0fba2b21a8e92e182ded62f3f
Michael Engel <mengel@redhat.com>
2024-02-14 16:36:47 +0100

    introduced a client class in integration tests
    Relates to: https://github.com/eclipse-bluechi/bluechi/issues/662
    
    Introduce a client class in integration tests to add an abstraction
    layer between tests and the used provision method - containerized or
    multiple hosts connected to via SSH.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- tests/bluechi_test/machine.py
+++ tests/bluechi_test/machine.py
@@ -1,14 +1,13 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-import io
 import logging
 import os
 import time
-import tarfile
+import traceback
 
-from podman.domain.containers import Container
-from typing import Any, Iterator, Optional, Tuple, Union, IO
+from typing import Any, Iterator, Optional, Tuple, Union
 
+from bluechi_test.client import Client, ContainerClient, SSHClient
 from bluechi_test.config import BluechiConfig, BluechiAgentConfig, BluechiControllerConfig
 from bluechi_test.util import read_file, get_random_name
 
@@ -20,83 +19,65 @@ valgrind_log_path_agent = '/var/log/valgrind/bluechi-agent-valgrind.log'
 
 class BluechiMachine():
 
-    def __init__(self, container: Container, config: BluechiConfig) -> None:
-        self.container: Container = container
+    def __init__(self, name: str, client: Client, config: BluechiConfig) -> None:
+        self.name = name
+        self.client = client
 
         # add confd file to container
-        self.create_file(config.get_confd_dir(), config.file_name, config.serialize())
+        self.client.create_file(config.get_confd_dir(), config.file_name, config.serialize())
 
     def create_file(self, target_dir: str, file_name: str, content: str) -> None:
-        content_encoded = content.encode("utf-8")
+        target_file = os.path.join(target_dir, file_name)
+        try:
+            self.client.create_file(target_dir, file_name, content)
+        except Exception as ex:
+            LOGGER.error(f"Failed to create file '{target_file}': {ex}")
+            traceback.print_exc()
+            return
 
-        buff: IO[bytes] = io.BytesIO()
-        with tarfile.open(fileobj=buff, mode="w:") as file:
-            info: tarfile.TarInfo = tarfile.TarInfo()
-            info.uid = 0
-            info.gid = 0
-            info.name = os.path.basename(file_name)
-            info.path = os.path.basename(file_name)
-            info.mode = 0o644
-            info.type = tarfile.REGTYPE
-            info.size = len(content_encoded)
-            file.addfile(info, fileobj=io.BytesIO(content_encoded))
-
-        buff.seek(0)
-        self.container.put_archive(path=target_dir, data=buff)
-
-    def get_file(self, container_path: str, local_path: str) -> None:
-        actual, _ = self.container.get_archive(container_path)
-
-        with io.BytesIO() as fd:
-            for chunk in actual:
-                fd.write(chunk)
-            fd.seek(0, 0)
-
-            with tarfile.open(fileobj=fd, mode="r") as tar:
-                tar.extractall(path=local_path)
-
-    def gather_journal_logs(self, data_dir: str) -> None:
-        log_file = f"/tmp/journal-{self.container.name}.log"
-
-        self.container.exec_run(
-            f'bash -c "journalctl --no-pager > {log_file}"', tty=True)
-
-        self.get_file(log_file, data_dir)
-
-    def gather_coverage(self, data_coverage_dir: str) -> None:
-        gcda_file_location = "/var/tmp/bluechi-coverage"
-        coverage_file = f"{gcda_file_location}/coverage-{self.container.name}.info"
-
-        LOGGER.info("Generating info file started")
-        result, output = self.exec_run(f"/usr/share/bluechi-coverage/bin/gather-code-coverage.sh {coverage_file}")
-        LOGGER.info("Generating info file finished")
-
-        self.get_file(f"{coverage_file}", data_coverage_dir)
-
-    def cleanup(self):
-        if self.container.status == 'running':
-            kw_params = {'timeout': 0}
-            self.container.stop(**kw_params)
-        self.container.remove()
+    def get_file(self, machine_path: str, local_path: str) -> None:
+        self.client.get_file(machine_path, local_path)
 
     def exec_run(self, command: (Union[str, list[str]]), raw_output: bool = False, tty: bool = True) -> \
             Tuple[Optional[int], Union[Iterator[bytes], Any, Tuple[bytes, bytes]]]:
+        return self.client.exec_run(command, raw_output, tty)
 
-        result, output = self.container.exec_run(command, tty=tty)
-        LOGGER.debug(f"Executed command '{command}' with result '{result}' and output '{output}'")
+    def gather_journal_logs(self, data_dir: str) -> None:
+        log_file = f"/tmp/journal-{self.name}.log"
 
-        if not raw_output and output:
-            output = output.decode('utf-8').strip()
+        self.client.exec_run(
+            f'bash -c "journalctl --no-pager > {log_file}"', tty=True)
 
-        return result, output
+        self.client.get_file(log_file, data_dir)
+
+    def gather_coverage(self, data_coverage_dir: str) -> None:
+        gcda_file_location = "/var/tmp/bluechi-coverage"
+        coverage_file = f"{gcda_file_location}/coverage-{self.name}.info"
+
+        LOGGER.info("Generating info file started")
+        result, output = self.client.exec_run(
+            f"/usr/share/bluechi-coverage/bin/gather-code-coverage.sh {coverage_file}")
+        LOGGER.info("Generating info file finished")
+
+        self.client.get_file(f"{coverage_file}", data_coverage_dir)
+
+    def cleanup(self):
+        if isinstance(self.client, ContainerClient):
+            if self.client.container.status == 'running':
+                kw_params = {'timeout': 0}
+                self.client.container.stop(**kw_params)
+            self.client.container.remove()
+        elif isinstance(self.client, SSHClient):
+            # TODO: implement proper cleanup (removing all added files etc.)
+            pass
 
     def systemctl_daemon_reload(self) -> Tuple[Optional[int], Union[Iterator[bytes], Any, Tuple[bytes, bytes]]]:
-        return self.exec_run("systemctl daemon-reload")
+        return self.client.exec_run("systemctl daemon-reload")
 
     def systemctl_start_and_wait(self, service_name: str, sleep_after: float = 0.0) -> \
             Tuple[Optional[int], Union[Iterator[bytes], Any, Tuple[bytes, bytes]], Optional[bool]]:
 
-        result, output = self.exec_run(f"systemctl start {service_name}")
+        result, output = self.client.exec_run(f"systemctl start {service_name}")
         wait_result = self.wait_for_unit_state_to_be(service_name, "active")
         time.sleep(sleep_after)
 
@@ -108,7 +89,7 @@ class BluechiMachine():
 
         LOGGER.debug(f"Copy local systemd service '{source_path}' to container path '{target_dir}'\
              with content:\n{content}")
-        self.create_file(target_dir, service_file_name, content)
+        self.client.create_file(target_dir, service_file_name, content)
         self.systemctl_daemon_reload()
 
     def copy_container_script(self, script_file_name: str):
@@ -120,14 +101,14 @@ class BluechiMachine():
 
         LOGGER.info(f"Copy container script '{source_path}' to container path '{curr_dir}'\
              with content:\n{content}")
-        self.create_file(target_dir, script_file_name, content)
+        self.client.create_file(target_dir, script_file_name, content)
 
     def service_is_active(self, unit_name: str) -> bool:
-        result, _ = self.exec_run(f"systemctl is-active {unit_name}")
+        result, _ = self.client.exec_run(f"systemctl is-active {unit_name}")
         return result == 0
 
     def get_unit_freezer_state(self, unit_name: str) -> str:
-        _, output = self.exec_run(f"systemctl show {unit_name} --property=FreezerState")
+        _, output = self.client.exec_run(f"systemctl show {unit_name} --property=FreezerState")
         state = str(output).split('=', 1)
         result = ""
         if len(state) > 1:
@@ -135,7 +116,7 @@ class BluechiMachine():
         return result
 
     def get_unit_state(self, unit_name: str) -> str:
-        _, output = self.exec_run(f"systemctl is-active {unit_name}")
+        _, output = self.client.exec_run(f"systemctl is-active {unit_name}")
         return output
 
     def _is_unit_in_state(self, unit_name: str, expected_state: str) -> bool:
@@ -163,14 +144,14 @@ class BluechiMachine():
         return False
 
     def enable_valgrind(self) -> None:
-        self.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/bin/valgrind -s --leak-check=yes "
-                      f"--log-file={valgrind_log_path_controller} /usr/libexec/bluechi-controller' "
-                      f"/usr/lib/systemd/system/bluechi-controller.service")
-        self.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/bin/valgrind -s --leak-check=yes "
-                      f"--log-file={valgrind_log_path_agent} /usr/libexec/bluechi-agent' "
-                      f"/usr/lib/systemd/system/bluechi-agent.service")
-        self.exec_run("mkdir -p /var/log/valgrind")
-        self.exec_run("systemctl daemon-reload")
+        self.client.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/bin/valgrind -s --leak-check=yes "
+                             f"--log-file={valgrind_log_path_controller} /usr/libexec/bluechi-controller' "
+                             f"/usr/lib/systemd/system/bluechi-controller.service")
+        self.client.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/bin/valgrind -s --leak-check=yes "
+                             f"--log-file={valgrind_log_path_agent} /usr/libexec/bluechi-agent' "
+                             f"/usr/lib/systemd/system/bluechi-agent.service")
+        self.client.exec_run("mkdir -p /var/log/valgrind")
+        self.client.exec_run("systemctl daemon-reload")
 
     def run_python(self, python_script_path: str) -> \
             Tuple[Optional[int], Union[Iterator[bytes], Any, Tuple[bytes, bytes]]]:
@@ -178,11 +159,11 @@ class BluechiMachine():
         target_file_dir = os.path.join("/", "tmp")
         target_file_name = get_random_name(10)
         content = read_file(python_script_path)
-        self.create_file(target_file_dir, target_file_name, content)
+        self.client.create_file(target_file_dir, target_file_name, content)
 
         target_file_path = os.path.join(target_file_dir, target_file_name)
         LOGGER.debug(f"Executing python script '{target_file_path}'")
-        result, output = self.exec_run(f'python3 {target_file_path}')
+        result, output = self.client.exec_run(f'python3 {target_file_path}')
         LOGGER.debug(f"Execute python script '{target_file_path}' finished with result '{result}' \
             and output:\n{output}")
         try:
@@ -191,13 +172,13 @@ class BluechiMachine():
             return result, output
 
     def extract_valgrind_logs(self, log_path: str, target_path: str, data_dir: str) -> None:
-        result, _ = self.exec_run(f'cp -f {log_path} {target_path}')
+        result, _ = self.client.exec_run(f'cp -f {log_path} {target_path}')
         if result == 0:
-            self.get_file(target_path, data_dir)
+            self.client.get_file(target_path, data_dir)
 
     def gather_valgrind_logs(self, data_dir: str) -> None:
-        bluechi_valgrind_filename = f"bluechi-controller-valgrind-{self.container.name}.log"
-        bluechi_agent_valgrind_filename = f"bluechi-agent-valgrind-{self.container.name}.log"
+        bluechi_valgrind_filename = f"bluechi-controller-valgrind-{self.name}.log"
+        bluechi_agent_valgrind_filename = f"bluechi-agent-valgrind-{self.name}.log"
         bluechi_valgrind_log_target_path = f"/tmp/{bluechi_valgrind_filename}"
         bluechi_agent_valgrind_log_target_path = f"/tmp/{bluechi_agent_valgrind_filename}"
 
@@ -206,17 +187,17 @@ class BluechiMachine():
         self.extract_valgrind_logs(valgrind_log_path_agent, bluechi_agent_valgrind_log_target_path, data_dir)
 
     def restart_with_config_file(self, config_file_location, service):
-        self.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/libexec/{service} -c "
-                      f"{config_file_location}' "
-                      f"/usr/lib/systemd/system/{service}.service")
-        self.exec_run("systemctl daemon-reload")
-        self.exec_run(f"systemctl restart {service}.service")
+        self.client.exec_run(f"sed -i '/ExecStart=/c\\ExecStart=/usr/libexec/{service} -c "
+                             f"{config_file_location}' "
+                             f"/usr/lib/systemd/system/{service}.service")
+        self.client.exec_run("systemctl daemon-reload")
+        self.client.exec_run(f"systemctl restart {service}.service")
 
 
 class BluechiAgentMachine(BluechiMachine):
 
-    def __init__(self, container: Container, config: BluechiAgentConfig) -> None:
-        super().__init__(container, config)
+    def __init__(self, name: str, client: Client, config: BluechiAgentConfig) -> None:
+        super().__init__(name, client, config)
 
         self.config = config
         self.node_name = config.node_name
@@ -233,8 +214,8 @@ class BluechiAgentMachine(BluechiMachine):
 
 class BluechiControllerMachine(BluechiMachine):
 
-    def __init__(self, container: Container, config: BluechiControllerConfig) -> None:
-        super().__init__(container, config)
+    def __init__(self, name: str, client: Client, config: BluechiControllerConfig) -> None:
+        super().__init__(name, client, config)
 
         self.config = config
 
@@ -250,7 +231,7 @@ class BluechiControllerMachine(BluechiMachine):
     def start_unit(self, node_name: str, unit_name: str) -> None:
         LOGGER.debug(f"Starting unit '{unit_name}' on node '{node_name}'")
 
-        result, output = self.exec_run(f"bluechictl start {node_name} {unit_name}")
+        result, output = self.client.exec_run(f"bluechictl start {node_name} {unit_name}")
         LOGGER.debug(f"Start unit '{unit_name}' on node '{node_name}' finished with result '{result}' \
             and output:\n{output}")
         if result != 0:
@@ -259,7 +240,7 @@ class BluechiControllerMachine(BluechiMachine):
     def stop_unit(self, node_name: str, unit_name: str) -> None:
         LOGGER.debug(f"Stopping unit '{unit_name}' on node '{node_name}'")
 
-        result, output = self.exec_run(f"bluechictl stop {node_name} {unit_name}")
+        result, output = self.client.exec_run(f"bluechictl stop {node_name} {unit_name}")
         LOGGER.debug(f"Stop unit '{unit_name}' on node '{node_name}' finished with result '{result}' \
             and output:\n{output}")
         if result != 0:
@@ -268,7 +249,7 @@ class BluechiControllerMachine(BluechiMachine):
     def enable_unit(self, node_name: str, unit_name: str) -> None:
         LOGGER.debug(f"Enabling unit '{unit_name}' on node '{node_name}'")
 
-        result, output = self.exec_run(f"bluechictl enable {node_name} {unit_name}")
+        result, output = self.client.exec_run(f"bluechictl enable {node_name} {unit_name}")
         LOGGER.debug(f"Enable unit '{unit_name}' on node '{node_name}' finished with result '{result}' \
             and output:\n{output}")
         if result != 0:
@@ -277,7 +258,7 @@ class BluechiControllerMachine(BluechiMachine):
     def freeze_unit(self, node_name: str, unit_name: str) -> None:
         LOGGER.debug(f"Freezing unit {unit_name} on node {node_name}")
 
-        result, output = self.exec_run(f"bluechictl freeze {node_name} {unit_name}")
+        result, output = self.client.exec_run(f"bluechictl freeze {node_name} {unit_name}")
         LOGGER.debug(f"Freeze unit '{unit_name}' on node '{node_name}' finished with result '{result}' \
             and output:\n{output}")
         if result != 0:
@@ -286,7 +267,7 @@ class BluechiControllerMachine(BluechiMachine):
     def thaw_unit(self, node_name: str, unit_name: str) -> None:
         LOGGER.debug(f"Thawing unit {unit_name} on node {node_name}")
 
-        result, output = self.exec_run(f"bluechictl thaw {node_name} {unit_name}")
+        result, output = self.client.exec_run(f"bluechictl thaw {node_name} {unit_name}")
         LOGGER.debug(f"Thaw unit '{unit_name}' on node '{node_name}' finished with result '{result}' \
             and output:\n{output}")
         if result != 0:
