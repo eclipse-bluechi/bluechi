9afa403e608c9805c8d730c75dda2b98295a5d75
Martin Perina <mperina@redhat.com>
2024-02-27 12:49:24 +0100

    Add test for bluechictl execution without parameters
    Adds a test, that bluechictl executed without parameters shows help and
    then exits with error result code.
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ tests/tests/tier0/bluechi-no-param-provided/test_no_param_provided.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from typing import Dict
+
+from bluechi_test.test import BluechiTest
+from bluechi_test.machine import BluechiControllerMachine, BluechiAgentMachine
+from bluechi_test.config import BluechiControllerConfig
+
+
+def check_help_option(ctrl: BluechiControllerMachine, _: Dict[str, BluechiAgentMachine]):
+    result, out = ctrl.exec_run("/usr/bin/bluechictl")
+    assert result != 0
+    assert 'Usage' in out
+
+
+def test_help_option_provided(bluechi_test: BluechiTest, bluechi_ctrl_default_config: BluechiControllerConfig):
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+
+    bluechi_test.run(check_help_option)
