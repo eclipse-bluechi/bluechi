c47749889ae6598c5631cf7ecdc91c6cdb57d716
Michael Engel <mengel@redhat.com>
2024-03-08 16:46:48 +0100

    added integration test for internal hello method
    Fixes: https://github.com/eclipse-bluechi/bluechi/issues/798
    
    Adding an integration test that verifies the internal API
    method 'Hello' works as expected.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/bluechi-internal-hello/python/call_internal_hello.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from dasbus.connection import AddressedMessageBus
+
+node = "node-foo"
+
+
+class TestListenerAddedAsPeer(unittest.TestCase):
+
+    def test_call_internal_hello(self):
+
+        port = ""
+        with open("/tmp/port", "r") as f:
+            port = f.read().strip()
+        assert port is not None
+
+        # connect to controller locally
+        peer_bus = AddressedMessageBus(f"tcp:host=127.0.0.1,port={port}")
+        peer_proxy = peer_bus.get_proxy(
+            service_name="org.eclipse.bluechi",
+            object_path="/org/freedesktop/DBus",
+            interface_name="org.freedesktop.DBus"
+        )
+        resp = peer_proxy.Hello()
+        assert resp == ":1.0"
+
+
+if __name__ == "__main__":
+    unittest.main()
