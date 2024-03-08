49db3aec01b51fa36ff47077ae7c5639e5f2cd61
Michael Engel <mengel@redhat.com>
2024-03-08 16:46:48 +0100

    added integration test for internal hello method
    Fixes: https://github.com/eclipse-bluechi/bluechi/issues/798
    
    Adding an integration test that verifies the internal API
    method 'Hello' works as expected.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/bluechi-internal-hello/test_bluechi_internal_hello.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import os
+
+from typing import Dict
+from bluechi_test.test import BluechiTest
+from bluechi_test.machine import BluechiControllerMachine, BluechiAgentMachine
+from bluechi_test.config import BluechiControllerConfig
+
+NODE_FOO = "node-foo"
+
+
+def exec(ctrl: BluechiControllerMachine, nodes: Dict[str, BluechiAgentMachine]):
+
+    # pass port information to machine
+    ctrl.create_file(os.path.join("/", "tmp"), "port", ctrl.config.port)
+
+    result, output = ctrl.run_python(os.path.join("python", "call_internal_hello.py"))
+    if result != 0:
+        raise Exception(output)
+
+
+def test_bluechi_internal_hello(bluechi_test: BluechiTest, bluechi_ctrl_default_config: BluechiControllerConfig):
+
+    bluechi_ctrl_default_config.allowed_node_names = [NODE_FOO]
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+
+    bluechi_test.run(exec)
