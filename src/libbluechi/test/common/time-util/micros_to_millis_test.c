13296df6f8e04020c5a94ca1152de3f4a22f8d85
Michael Engel <mengel@redhat.com>
2024-03-14 09:25:51 +0100

    added unit test for micros_to_millis
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ src/libbluechi/test/common/time-util/micros_to_millis_test.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#include <assert.h>
+#include <stdlib.h>
+
+#include "libbluechi/common/time-util.h"
+
+
+int main() {
+        assert(micros_to_millis(0) == 0);
+        assert(micros_to_millis(12) == 0.012);
+        assert(micros_to_millis(UINT64_MAX) == 18446744073709552);
+        return EXIT_SUCCESS;
+}
