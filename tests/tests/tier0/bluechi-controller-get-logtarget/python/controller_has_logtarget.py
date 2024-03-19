81f8b20eecc754c53e8a24ed4cf14d085d93819d
Michael Engel <mengel@redhat.com>
2024-03-19 16:28:47 +0100

    added integration tests for getting logtarget
    Fixes: https://github.com/eclipse-bluechi/bluechi/issues/786
    Fixes: https://github.com/eclipse-bluechi/bluechi/issues/791
    
    Added integration tests to verify that the logtarget property
    of the controller and agent are configured and can be
    retrieved via D-Bus API property.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ tests/tests/tier0/bluechi-controller-get-logtarget/python/controller_has_logtarget.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import unittest
+
+from bluechi.api import Controller
+
+expected_logtarget = "stderr-full"
+
+
+class TestHasLogTarget(unittest.TestCase):
+
+    def test_controller_has_logtarget(self):
+        controller = Controller()
+        assert controller.log_target == expected_logtarget
+
+
+if __name__ == "__main__":
+    unittest.main()
