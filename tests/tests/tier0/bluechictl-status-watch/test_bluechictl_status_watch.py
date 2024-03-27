358f44701c74e60fcdf8b770f5fa73b0eb149368
Mark Kemel <mkemel@redhat.com>
2024-03-27 17:19:16 +0200

    test bluechictl status -w
    Add a test that covers the functions handling the
    `bluechictl status -w` command
    
    Fixes: https://github.com/eclipse-bluechi/bluechi/issues/785
    Signed-off-by: Mark Kemel <mkemel@redhat.com>

---
+++ tests/tests/tier0/bluechictl-status-watch/test_bluechictl_status_watch.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+import os
+import logging
+from typing import Dict
+
+from bluechi_test.config import BluechiControllerConfig, BluechiAgentConfig
+from bluechi_test.machine import BluechiControllerMachine, BluechiAgentMachine
+from bluechi_test.test import BluechiTest
+
+LOGGER = logging.getLogger(__name__)
+
+node_foo_name = "node-foo"
+
+
+def exec(ctrl: BluechiControllerMachine, nodes: Dict[str, BluechiAgentMachine]):
+    result, output = ctrl.run_python(os.path.join("python", "bluechictl_status_watch.py"))
+    assert result == 0
+
+
+def test_bluechictl_status_watch(
+        bluechi_test: BluechiTest,
+        bluechi_ctrl_default_config: BluechiControllerConfig,
+        bluechi_node_default_config: BluechiAgentConfig):
+
+    node_foo_cfg = bluechi_node_default_config.deep_copy()
+    node_foo_cfg.node_name = node_foo_name
+
+    bluechi_ctrl_default_config.allowed_node_names = [node_foo_name]
+
+    bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
+    bluechi_test.add_bluechi_agent_config(node_foo_cfg)
+
+    bluechi_test.run(exec)
