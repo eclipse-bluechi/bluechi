417c4b3f0ba047124d656cb6c132c2aceb920a80
Michael Engel <mengel@redhat.com>
2024-10-17 16:52:49 +0200

    Implemented bluechi-is-online tool
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ src/is-online/help.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright Contributors to the Eclipse BlueChi project
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+#include "help.h"
+#include "opt.h"
+
+static void usage_print_header() {
+        printf("bluechi-is-online checks and monitors the connection state of BlueChi components\n");
+        printf("\n");
+}
+
+static void usage_print_usage(const char *usage) {
+        printf("Usage: \n");
+        printf("  %s\n", usage);
+        printf("\n");
+}
+
+
+void usage() {
+        usage_print_header();
+        usage_print_usage("bluechi-is-online [agent|node|system] [OPTIONS]");
+        printf("Available commands:\n");
+        printf("  help: \t shows this help message\n");
+        printf("  version: \t shows the version of bluechi-is-online\n");
+        printf("Available options:\n");
+        printf("  --%s: \t keeps monitoring as long as [agent|node|system] is online and exits if it detects an offline state.\n",
+               ARG_MONITOR);
+        printf("  --%s: \t Wait n milliseconds for [agent|node|system] to get online.\n", ARG_WAIT);
+        printf("  --%s: \t Only for [agent]. Wait n milliseconds for after switching controller to get online again.\n",
+               ARG_SWITCH_CTRL_TIMEOUT);
+}
+
+int method_help(UNUSED Command *command, UNUSED void *userdata) {
+        usage();
+        return 0;
+}
---
+++ src/is-online/help.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright Contributors to the Eclipse BlueChi project
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+#pragma once
+
+#include "libbluechi/cli/command.h"
+
+int method_help(Command *command, void *userdata);
+void usage();
