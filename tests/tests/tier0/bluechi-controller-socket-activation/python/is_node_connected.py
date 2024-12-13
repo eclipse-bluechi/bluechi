b56913928e24407ea5cf27036dd8dc53950713ae
Michael Engel <mengel@redhat.com>
2024-12-13 11:52:17 +0100

    Fix flacky node is online subtest
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- tests/tests/tier0/bluechi-controller-socket-activation/python/is_node_connected.py
+++ tests/tests/tier0/bluechi-controller-socket-activation/python/is_node_connected.py
@@ -15,11 +15,15 @@ NODE_NAME = "node-foo"
 
 class TestNodeIsConnected(unittest.TestCase):
     def test_node_is_connected(self):
-        n = Node(NODE_NAME)
 
         with Timeout(5, f"Timeout while waiting for agent '{NODE_NAME}' to be online"):
-            while n.status != "online":
-                time.sleep(0.5)
+            status = "offline"
+            while status != "online":
+                try:
+                    status = Node(NODE_NAME).status
+                    time.sleep(0.5)
+                except Exception:
+                    pass
 
 
 if __name__ == "__main__":
