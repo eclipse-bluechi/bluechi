c3ed8c60f81f8a3abd9c34ea38f37252173df3f0
yaniwang <yaniwang@redhat.com>
2025-03-17 16:59:52 +0800

    Based on the suggestions, the logic to start local agent has been modified.

--- tests/tests/tier0/bluechi-and-agent-on-same-machine/test_bluechi_and_agent_on_same_machine.py
+++ tests/tests/tier0/bluechi-and-agent-on-same-machine/test_bluechi_and_agent_on_same_machine.py
@@ -16,23 +16,7 @@ node_foo_name = "node-foo"
 def exec(ctrl: BluechiControllerMachine, nodes: Dict[str, BluechiAgentMachine]):
 
     assert ctrl.wait_for_unit_state_to_be("bluechi-controller", "active")
-
-    node_foo_config = BluechiAgentConfig(
-        file_name="agent.conf",
-        node_name=node_foo_name,
-        controller_host="localhost",
-        controller_port=ctrl.config.port,
-    )
-    ctrl.create_file(
-        node_foo_config.get_confd_dir(),
-        node_foo_config.file_name,
-        node_foo_config.serialize(),
-    )
-    result, _, wait_result = ctrl.systemctl_start_and_wait("bluechi-agent", 1)
-    assert result == 0
-    assert wait_result
-
-    # bluechi-agent is running, check if it is connected
+    # local bluechi-agent is running, check if it is connected
     result, output = ctrl.run_python(os.path.join("python", "is_node_connected.py"))
     if result != 0:
         raise Exception(output)
@@ -47,4 +31,11 @@ def test_bluechi_and_agent_on_same_machine(
     bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
     # don't add node_foo_config to bluechi_test to prevent it being started
     # as separate container
+    bluechi_test.set_bluechi_local_agent_config(
+        BluechiAgentConfig(
+            controller_address="unix:path=/run/bluechi/bluechi.sock",
+            node_name=node_foo_name,
+            file_name="agent.conf",
+        ).deep_copy()
+    )
     bluechi_test.run(exec)
