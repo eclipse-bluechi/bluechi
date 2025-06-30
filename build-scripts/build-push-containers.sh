bc40a7904af3563ac3c0b5040a436d878cc682c4
Michael Engel <mengel@redhat.com>
2025-06-30 16:50:06 +0200

    Added container build arg to build for cs9 as well as cs10
    Relates to: https://github.com/eclipse-bluechi/bluechi/issues/1086
    
    In order to be able to build and test on CentOS Stream 10 while
    simultaneously supporting CentOS Stream 9 builds and tests for
    backport, the CentOS Stream version is being parameterized and
    the GH Action for building and pushing the images have been
    updated. It also swaps the build-base:latest with :stream10
    image in the GH CI.
    
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- build-scripts/build-push-containers.sh
+++ build-scripts/build-push-containers.sh
@@ -7,28 +7,35 @@
 # The 1st parameter is the image name
 IMAGE="$1"
 
-# The 2nd parameter is optional and it specifies the container architecture. If omitted, all archs will be built.
-ARCHITECTURES="${2:-amd64 arm64}"
+# The 2nd parameter is the centos stream version
+CENTOS_STREAM_VERSION="$2"
+
+# The 3rd parameter is optional and it specifies the container architecture. If omitted, all archs will be built.
+ARCHITECTURES="${3:-amd64 arm64}"
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=$SCRIPT_DIR"/../"
 CONTAINER_FILE_DIR=$SCRIPT_DIR"/../containers"
 
+IMAGE_NAME="${IMAGE}:${CENTOS_STREAM_VERSION}"
+QUAY_NAME="quay.io/bluechi/${IMAGE_NAME}"
+
 function push(){
-    buildah manifest push --all $IMAGE "docker://quay.io/bluechi/$IMAGE"
+    buildah manifest push --all "${IMAGE_NAME}" "docker://${QUAY_NAME}"
 }
 
 function build(){
     # remove old image, ignore result
-    buildah manifest rm $IMAGE &> /dev/null || true
+    buildah manifest rm "${IMAGE_NAME}" &> /dev/null || true
 
-    buildah manifest create $IMAGE
+    buildah manifest create "${IMAGE_NAME}"
 
     for arch in $ARCHITECTURES; do
-        buildah bud --tag "quay.io/bluechi/$IMAGE" \
-            --manifest $IMAGE \
-            --arch ${arch} \
-            -f ${CONTAINER_FILE_DIR}/${IMAGE} \
-            ${ROOT_DIR}
+        buildah bud --tag "${QUAY_NAME}" \
+            --manifest "${IMAGE_NAME}" \
+            --arch "${arch}" \
+            --build-arg "centos_stream_version=${CENTOS_STREAM_VERSION}" \
+            -f "${CONTAINER_FILE_DIR}/${IMAGE}" \
+            "${ROOT_DIR}"
     done
 }
 
