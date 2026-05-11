06c2d26356898a078e8ca51e581765ceb07e6985
Michael Engel <mengel@redhat.com>
2026-05-11 12:21:20 +0200

    Fixed race condition in agent invalid port config integration test
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- tests/tests/tier0/bluechi-agent-invalid-port-configuration/test_agent_invalid_port_configuration.py
+++ tests/tests/tier0/bluechi-agent-invalid-port-configuration/test_agent_invalid_port_configuration.py
@@ -10,19 +10,25 @@ from bluechi_test.machine import BluechiAgentMachine, BluechiControllerMachine
 from bluechi_test.test import BluechiTest
 
 NODE_FOO = "node-foo"
-NODE_BAR = "node-bar"
 
 
 def start_with_invalid_port(
     ctrl: BluechiControllerMachine, nodes: Dict[str, BluechiAgentMachine]
 ):
-    node_foo_with_valid_port = nodes[NODE_FOO]
-    assert node_foo_with_valid_port.wait_for_unit_state_to_be("bluechi-agent", "active")
+    node_foo = nodes[NODE_FOO]
+    assert node_foo.wait_for_unit_state_to_be("bluechi-agent", "active")
 
-    node_bar_with_invalid_port = nodes[NODE_BAR]
-    assert node_bar_with_invalid_port.wait_for_unit_state_to_be(
-        "bluechi-agent", "failed"
+    # Use letter O instead of number 0 in controller port string
+    # to mimic a typo
+    node_foo.config.controller_port = "842O"
+    node_foo.create_file(
+        node_foo.config.get_confd_dir(),
+        node_foo.config.file_name,
+        node_foo.config.serialize(),
     )
+
+    node_foo.systemctl.restart_unit("bluechi-agent.service")
+    assert node_foo.wait_for_unit_state_to_be("bluechi-agent", "failed")
 
 
 def test_agent_invalid_port_configuration(
@@ -35,14 +41,9 @@ def test_agent_invalid_port_configuration(
     node_foo_cfg.node_name = NODE_FOO
     node_foo_cfg.controller_port = "8420"
 
-    node_bar_cfg = bluechi_node_default_config.deep_copy()
-    node_bar_cfg.node_name = NODE_BAR
-    node_bar_cfg.controller_port = "842O"
-
-    bluechi_ctrl_default_config.allowed_node_names = [NODE_FOO, NODE_BAR]
+    bluechi_ctrl_default_config.allowed_node_names = [NODE_FOO]
     bluechi_test.set_bluechi_controller_config(bluechi_ctrl_default_config)
 
     bluechi_test.add_bluechi_agent_config(node_foo_cfg)
-    bluechi_test.add_bluechi_agent_config(node_bar_cfg)
 
     bluechi_test.run(start_with_invalid_port)
